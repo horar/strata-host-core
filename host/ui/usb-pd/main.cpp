7ecb51f7ee91b43088bd9d873f494eec8a0365e2
Prasanth.Vivek <prasanth.vivekanandan@onsemi.com>
2018-01-30 15:14:10 -0800

    1) Modified the main.cpp in ui to launch the HCS before UI 2) Add forwarded channel to print the HCS log on QT terminal

--- host/ui/usb-pd/main.cpp
+++ host/ui/usb-pd/main.cpp
@@ -24,6 +24,65 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    // Starting services this build?
+    // [prasanth] : Important note: Start HCS before launching the UI
+    // So the service callback works properly
+    #define START_SERVICES
+    #ifdef START_SERVICES
+
+        #ifdef Q_OS_WIN
+        // We are at the build folder as root
+        #define HOST_ROOT_PATH      (app.applicationDirPath())
+        #define HCS_PATH            HOST_ROOT_PATH + "HCS/HCS.exe"
+        #define HCS_CONFIG_PATH     HOST_ROOT_PATH + "HCS/host_controller_service.config"
+        #endif
+
+        #ifdef Q_OS_MACOS
+        // We are pretty deep in the directory. Ex. ui/build-xxx-Release/spyglass.app/Contents/MacOs
+        #define HOST_ROOT_PATH      (app.applicationDirPath() + "/../../../../../")
+        #define HCS_PATH            HOST_ROOT_PATH + "HostControllerService/build/hcs"
+        #define HCS_CONFIG_PATH     HOST_ROOT_PATH + "HostControllerService/files/conf/host_controller_service.config"
+        #endif
+
+        #ifdef Q_OS_LINUX
+        // We are at the build folder
+        #define HOST_ROOT_PATH      (app.applicationDirPath() + "/../../")
+        #define HCS_PATH            HOST_ROOT_PATH + "HostControllerService/build/hcs"
+        #define HCS_CONFIG_PATH     HOST_ROOT_PATH + "HostControllerService/files/conf/host_controller_service.config"
+        #endif
+
+        /* This is the same across all platforms
+        */
+
+        // Ensure HCS exists
+        QFileInfo hcs_file(HCS_PATH);
+        bool hcs_started = false;
+
+        // Create a QProcess
+        QProcess *hcsProcess = new QProcess(nullptr);
+
+        if(hcs_file.exists()) {
+            hcs_started = true;
+
+            // Start HCS before handling events for QT
+            qDebug() << "Starting HCS: " << HCS_PATH;
+            QString hcsPath = HCS_PATH;
+
+            // Argument list for HCS
+            QStringList arguments;
+            arguments << "-f" << HCS_CONFIG_PATH;
+
+            // Start HCS
+            hcsProcess->setProcessChannelMode(QProcess::ForwardedChannels);
+            hcsProcess->start(hcsPath,arguments, QIODevice::ReadWrite);
+        }
+        else {
+            hcs_started = false;
+            qDebug() << "Failed to start HCS: Does not exist";
+        }
+
+    #endif
+
     qmlRegisterType<ImplementationInterfaceBinding>("tech.spyglass.ImplementationInterfaceBinding",1,0,"ImplementationInterfaceBinding");
     qmlRegisterType<Document>("tech.spyglass.Document", 1, 0, "Document");
     qmlRegisterType<DocumentManager>("tech.spyglass.DocumentManager", 1, 0, "DocumentManager");
@@ -43,63 +102,6 @@ int main(int argc, char *argv[])
     if (engine.rootObjects().isEmpty()) {
         return -1;
     }
-
-// Starting services this build?
-#ifdef START_SERVICES
-
-    #ifdef Q_OS_WIN
-    // We are at the build folder as root
-    #define HOST_ROOT_PATH      (app.applicationDirPath())
-    #define HCS_PATH            HOST_ROOT_PATH + "HCS/HCS.exe"
-    #define HCS_CONFIG_PATH     HOST_ROOT_PATH + "HCS/host_controller_service.config"
-    #endif
-
-    #ifdef Q_OS_MACOS
-    // We are pretty deep in the directory. Ex. ui/build-xxx-Release/spyglass.app/Contents/MacOs
-    #define HOST_ROOT_PATH      (app.applicationDirPath() + "/../../../../../")
-    #define HCS_PATH            HOST_ROOT_PATH + "HostControllerService/build/hcs"
-    #define HCS_CONFIG_PATH     HOST_ROOT_PATH + "HostControllerService/files/conf/host_controller_service.config"
-    #endif
-
-    #ifdef Q_OS_LINUX
-    // We are at the build folder
-    #define HOST_ROOT_PATH      (app.applicationDirPath() + "/../../")
-    #define HCS_PATH            HOST_ROOT_PATH + "HostControllerService/build/hcs"
-    #define HCS_CONFIG_PATH     HOST_ROOT_PATH + "HostControllerService/files/conf/host_controller_service.config"
-    #endif
-
-    /* This is the same across all platforms
-    */
-
-    // Ensure HCS exists
-    QFileInfo hcs_file(HCS_PATH);
-    bool hcs_started = false;
-
-    // Create a QProcess
-    QProcess *hcsProcess = new QProcess(nullptr);
-
-    if(hcs_file.exists()) {
-        hcs_started = true;
-
-        // Start HCS before handling events for QT
-        qDebug() << "Starting HCS: " << HCS_PATH;
-        QString hcsPath = HCS_PATH;
-
-        // Argument list for HCS
-        QStringList arguments;
-        arguments << "-f" << HCS_CONFIG_PATH;
-
-        // Start HCS
-        hcsProcess->setStandardOutputFile("hcs_output.log");
-        hcsProcess->setStandardErrorFile("hcs_error.log");
-        hcsProcess->start(hcsPath,arguments, QIODevice::ReadWrite);
-    }
-    else {
-        hcs_started = false;
-        qDebug() << "Failed to start HCS: Does not exist";
-    }
-
-#endif
 
     // Call QT and stay here until the application quits.
     int appResult = app.exec();
