0236e82e3bef1930baddc63d66a5e26ba7d92c69
Luay Alshawi <luay.alshawi@onsemi.com>
2018-10-25 21:55:34 -0700

    CBL: test file

---
+++ host/CouchBaseLite/test/test.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Luay Alshawi on 10/25/18.
+//
+#include <iostream>
+#include "CBLDatabase.h"
+using namespace std;
+int main(){
+
+    CBLDatabase cblDatabase("mydb");
+    return 0;
+}
