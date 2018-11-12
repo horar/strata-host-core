d1b73fbab3576ccbc53050a3dcbf5a9fdfe2a15d
Milan Franc <milan.franc@onsemi.com>
2018-11-12 16:35:30 +0100

    WIP: created libs folder and moved connector and flasher libs to it, created ext_libs folder for external libraries - libserial, libevent, libzmq

---
+++ host/CMakeModules/FindSerialPort.cmake
@@ -0,0 +1,36 @@
+if( TARGET serialport )
+    return()
+endif()
+
+unset( SERIAL_PORT_LIBRARIES CACHE )
+unset( SERIAL_PORT_LIBRARIES )
+unset( SERIAL_PORT_INCLUDE_DIR CACHE )
+unset( SERIAL_PORT_INCLUDE_DIR )
+
+message( STATUS "Looking for Serial port library" )
+
+set(EXT_LIBS_PATH ${CMAKE_SOURCE_DIR}/ext_libs)
+
+# TODO: add other platforms...
+if (APPLE)
+  set(PLATFORM_TYPE "mac")
+endif()
+
+find_library( SERIAL_PORT_LIBRARIES NAMES "serialport" PATHS ${EXT_LIBS_PATH}/libserial/lib/${PLATFORM_TYPE} )
+find_path( SERIAL_PORT_INCLUDE_DIR "libserialport.h" PATHS ${EXT_LIBS_PATH}/libserial/include )
+if( NOT SERIAL_PORT_INCLUDE_DIR )
+   message( FATAL_ERROR "Serial port includes not found")
+endif()
+if( NOT SERIAL_PORT_LIBRARIES )
+   message( FATAL_ERROR "Serial port libraries not found")
+endif()
+
+message( STATUS "Serial port found at ${SERIAL_PORT_LIBRARIES}" )
+
+add_library( serialport UNKNOWN IMPORTED )
+set_property( TARGET serialport PROPERTY IMPORTED_LOCATION "${SERIAL_PORT_LIBRARIES}" )
+set_property( TARGET serialport PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${SERIAL_PORT_INCLUDE_DIR}" )
+
+mark_as_advanced(
+    SERIAL_PORT_LIBRARIES
+    SERIAL_PORT_INCLUDE_DIR )
---
+++ host/CMakeModules/FindZeroMQ.cmake
@@ -0,0 +1,36 @@
+if( TARGET ZeroMQ )
+    return()
+endif()
+
+unset( ZEROMQ_LIBRARIES CACHE )
+unset( ZEROMQ_LIBRARIES )
+unset( ZEROMQ_INCLUDE_DIR CACHE )
+unset( ZEROMQ_INCLUDE_DIR )
+
+message( STATUS "Looking for ZeroMQ library" )
+
+set(EXT_LIBS_PATH ${CMAKE_SOURCE_DIR}/ext_libs)
+
+# TODO: add other platforms...
+if (APPLE)
+  set(PLATFORM_TYPE "mac")
+endif()
+
+find_library( ZEROMQ_LIBRARIES NAMES "zmq" PATHS ${EXT_LIBS_PATH}/libzmq/lib/${PLATFORM_TYPE} )
+find_path( ZEROMQ_INCLUDE_DIR "zmq.h" PATHS ${EXT_LIBS_PATH}/libzmq/include )
+if( NOT ZEROMQ_INCLUDE_DIR )
+   message( FATAL_ERROR "ZeroMQ includes not found")
+endif()
+if( NOT SERIAL_PORT_LIBRARIES )
+   message( FATAL_ERROR "ZeroMQ libraries not found")
+endif()
+
+message( STATUS "ZeroMQ found at ${ZEROMQ_LIBRARIES}" )
+
+add_library( ZeroMQ UNKNOWN IMPORTED )
+set_property( TARGET ZeroMQ PROPERTY IMPORTED_LOCATION "${ZEROMQ_LIBRARIES}" )
+set_property( TARGET ZeroMQ PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${ZEROMQ_INCLUDE_DIR}" )
+
+mark_as_advanced(
+    ZEROMQ_LIBRARIES
+    ZEROMQ_INCLUDE_DIR )
