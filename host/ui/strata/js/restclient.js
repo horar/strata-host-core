ca7f623e047bb0ece9706160a66aab6c7599dd23
David Faller <david.faller@onsemi.com>
2018-12-04 20:08:57 +0000

    Merged in SCT-123-strata-login-feedback (pull request #368)
    SCT-123 strata login feedback
    
    * added status updates for login connection, UI will now display all states and updated errors
    
    * cleaning up code and commenting unnecessary console logs
    
    Approved-by: Prasanth Vivek <prasanth.vivekanandan@onsemi.com>
    Approved-by: Ian Cain <zbdmxc@onsemi.com>

--- host/ui/strata/js/restclient.js
+++ host/ui/strata/js/restclient.js
@@ -1,30 +1,49 @@
 .pragma library
+
 var url = "http://18.222.75.160/";
+
 // jwt will be set after successful login
 var jwt = '';
-var xhr = function(method, endpoint, data, callback,errorCallback) {
+
+var xhr = function(method, endpoint, data, callback, errorCallback, signals) {
+
     var xhr = new XMLHttpRequest();
+
+    if (signals) {
+        signals.connectionStatus(xhr.readyState)  // Send connection status updates to UI
+    }
+
     xhr.onreadystatechange = function() {
             if ( xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300) {
-                console.log(xhr.responseText)
+                //console.log(xhr.responseText)
                 callback( JSON.parse(xhr.responseText) );
             }
-            else if(xhr.readyState === 4 && xhr.status >= 400){
-                if(errorCallback !== undefined){
+            else if (xhr.readyState === 4 && xhr.status >= 300) {
+                if (errorCallback) {
                     errorCallback(JSON.parse(xhr.responseText));
                 }
             }
-            // No connection to db. For some reason we get readyState as 4.
-            else if(xhr.readyState === 4 && !xhr.hasOwnProperty("status") ){
-                errorCallback({status: "no connection"})
+            // No connection to db - readyState is 4 (request complete)
+            else if (xhr.readyState === 4 && !xhr.hasOwnProperty("status")) {
+                errorCallback({message: "No connection"})
+            }
+            // Send connection status updates to UI
+            else if (signals) {
+                signals.connectionStatus(xhr.readyState)
             }
         };
-        var fullUrl = url + endpoint;
-        xhr.open( method, fullUrl );
-        // This must be after open
-        xhr.setRequestHeader("Content-Type","application/json");
-        // Set JWT in the requst header
+
+    var fullUrl = url + endpoint;
+    xhr.open( method, fullUrl );
+
+    // This must be after open
+    xhr.setRequestHeader("Content-Type","application/json");
+
+    // Set JWT in the requst header
+    if (jwt !== '') {
         console.log("JWT", jwt)
         xhr.setRequestHeader("x-access-token",jwt);
-        xhr.send(JSON.stringify(data));
+    }
+
+    xhr.send(JSON.stringify(data));
 }
