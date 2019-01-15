7f3729f63f1e57dc0459e46023581c6a2cb26820
Lubomir Carik <Lubomir.Carik@onsemi.com>
2019-01-15 19:09:39 +0100

    build: keep unify property/target names
    We checked 'ZeroMQ_Helper' targetm then create 'ZMQ_Helper' target/library;
    define 'ZMQHELPER_FOUND' variable and the module name was
    'FindZMQHelper'.
    Not all these muss match in complex project but at least some have to
    e.g. moduel name and _FOUND property...

--- host/CMakeModules/FindZMQHelper.cmake
+++ host/CMakeModules/FindZMQHelper.cmake
@@ -1,4 +1,4 @@
-if( TARGET ZeroMQ_Helper )
+if( TARGET ZMQHelper )
     return()
 endif()
 
@@ -14,20 +14,20 @@ set(EXT_LIBS_PATH ${CMAKE_SOURCE_DIR}/ext_libs)
 find_path( ZMQHELP_INCLUDE_DIR "zmq.hpp" PATHS ${EXT_LIBS_PATH}/zmq/include )
 
 if (ZMQHELP_INCLUDE_DIR)
-    set(ZMQHELPER_FOUND TRUE)
+    set(ZMQHelper_FOUND TRUE)
 endif()
 
-if (ZMQHELPER_FOUND)
+if (ZMQHelper_FOUND)
 
-    message( STATUS "ZMQ_Helper headers found at ${ZMQHELP_INCLUDE_DIR}" )
+    message( STATUS "ZMQHelper headers found at ${ZMQHELP_INCLUDE_DIR}" )
 
-    add_library( ZMQ_Helper UNKNOWN IMPORTED )
-    set_property( TARGET ZMQ_Helper PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${ZMQHELP_INCLUDE_DIR}" )
+    add_library( ZMQHelper UNKNOWN IMPORTED )
+    set_property( TARGET ZMQHelper PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${ZMQHELP_INCLUDE_DIR}" )
 
     mark_as_advanced(
             ZMQHELP_INCLUDE_DIR )
 else()
-    if (ZMQHELPER_FIND_REQUIRED)
-        message(FATAL_ERROR "Could NOT find ZeroMQ_Helper development files: ${ZMQHELP_INCLUDE_DIR} ")
+    if (ZMQHelper_FIND_REQUIRED)
+        message(FATAL_ERROR "Could NOT find ZMQHelper development files: ${ZMQHELP_INCLUDE_DIR} ")
     endif()
 endif()
