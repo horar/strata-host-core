3b3291bd506a795136fa0d4816cff67b2a853d71
Ondrej Kucharik <ondrej.kucharik@onsemi.com>
2019-02-04 15:55:26 +0100

    pull request fix 6

--- host/apps/Flasher/flasher-cli/src/main.cpp
+++ host/apps/Flasher/flasher-cli/src/main.cpp
@@ -21,39 +21,27 @@
 #include <Connector.h>
 #include <Flasher.h>
 
-using namespace std;
 
-#define MANUAL_OPEN_PORT 0
 int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
-		cout << "Usage: ./flasher <path_to_firmware.bin>" << endl;
+        std::cout << "Usage: ./flasher <path_to_firmware.bin>" << std::endl;
 		return 1;
 	}
 
-	char *firmware_file_path = argv[1];
+    const char* firmware_file_path = argv[1];
 
     std::unique_ptr<Connector> connector(ConnectorFactory::getConnector("platform"));
-#if MANUAL_OPEN_PORT
-    if(!connector->open("/dev/cu.usbserial-DB00VFH8"))
-    {
-        return 1;
-    }
-#endif
+
     Flasher flasher(connector.get(), firmware_file_path);
 
-    cout << "START: flash" << endl;
+    std::cout << "START: flash" << std::endl;
 
-    if (false == flasher.initializeBootloader())
-    {
-        return 1;
-    }
+    bool result = flasher.initializeBootloader() && flasher.flash(true);
 
-    bool result = flasher.flash(true);
+    std::cout << "Flash: Return Status:   " << ( result ? "OK": "Failed" ) << std::endl;
+    std::cout << "END: flash" << std::endl;
 
-    cout << "Flash: Return Status:   " << ( result ? "OK": "Failed" ) << endl;
-    cout << "END: flash" << endl;
-
-    return 0;
+    return ( result ? 0 : 1 );
 }
