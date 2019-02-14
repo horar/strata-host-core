736ae0f7b47664f3b306cf6c977aeb94297f9e5f
Milan Franc <milan.franc@onsemi.com>
2019-02-14 14:29:09 +0100

    updated sandbox test program

--- host/utils/platform_mgr_test/main.cpp
+++ host/utils/platform_mgr_test/main.cpp
@@ -49,37 +49,12 @@ int main(int argc, char* argv[])
 
     mgr->StartLoop();
 
-    bool loop = true;
-    while(loop) {
+    //make some loops and then exit..
+    for(int i=0; i<5; i++) {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
     mgr->Stop();
-
-
-
-#if 0
-    std::vector<std::string> listOfPorts;
-    getListOfSerialPorts(listOfPorts);
-
-    if (listOfPorts.empty())
-        return 1;
-
-    EvEventsMgr ev_mgr;
-
-    PlatformConnection* conn = new PlatformConnection(mgr);
-    bool ret = conn->open(listOfPorts.front());
-    if (ret) {
-        conn->attachEventMgr(&ev_mgr);
-
-    }
-
-
-    conn->addMessage("{\"cmd\":\"request_platform_id\",\"payload\":{} }");
-
-#endif
-
-
 
     return 0;
 }
