49f139473ffd36da3ffe4d114fa9e194d0fd2372
Milan Franc <milan.franc@onsemi.com>
2019-04-16 14:57:31 +0200

    reordered member variables initialization

--- host/libs/EventsMgr/src/EvEvent.cpp
+++ host/libs/EventsMgr/src/EvEvent.cpp
@@ -37,8 +37,8 @@ EvEvent::EvEvent() : EvEvent(EvType::eEvTypeUnknown, (ev_handle_t) - 1, 0)
 }
 
 EvEvent::EvEvent(EvType type, ev_handle_t fileHandle, unsigned int timeInMs) : EvEventBase(type),
-        fileHandle_{fileHandle},
-        timeInMs_{timeInMs}
+       timeInMs_{timeInMs},
+       fileHandle_{fileHandle}
 {
 }
 
