2d8f6c212ac5585ff97af9c3bde23e562b1f3aaa
Luay Alshawi <luay.alshawi@onsemi.com>
2019-04-19 17:44:34 -0700

    Install hcs.config in ProgramData directory.  Using Windows API to find ProgramData and build hcs.config path to pass it to hcs

--- host/apps/strata/main.cpp
+++ host/apps/strata/main.cpp
@@ -14,6 +14,10 @@
 #include "QtDebug"
 #include <QProcess>
 #include <QSettings>
+#ifdef Q_OS_WIN
+#include <Shlwapi.h>
+#include <ShlObj.h>
+#endif
 
 #include <PlatformInterface/core/CoreInterface.h>
 
@@ -82,7 +86,14 @@ int main(int argc, char *argv[])
 #ifdef Q_OS_WIN
 #if WINDOWS_INSTALLER_BUILD
     const QString hcsPath{ QDir::cleanPath(QString("%1/HCS/hcs2.exe").arg(app.applicationDirPath())) };
-    const QString hcsConfigPath{ QDir::cleanPath(QString("%1/HCS/hcs.config").arg(app.applicationDirPath()))};
+	QString hcsConfigPath;
+	TCHAR programDataPath[MAX_PATH];
+    if (SUCCEEDED(SHGetFolderPath(NULL, CSIDL_COMMON_APPDATA, NULL, 0, programDataPath))) {
+		hcsConfigPath = QDir::cleanPath(QString("%1/ON Semiconductor/Strata/HCS/hcs.config").arg(programDataPath));
+		qCInfo(logCategoryStrataDevStudio) << QStringLiteral("hcsConfigPath:") << hcsConfigPath ;
+    }else{
+		qCCritical(logCategoryStrataDevStudio) << "Failed to get ProgramData path using windows API call...";
+	}
 #else
      const QString hcsPath{ QDir::cleanPath(QString("%1/hcs2.exe").arg(app.applicationDirPath())) };
      const QString hcsConfigPath{ QDir::cleanPath(QString("%1/../../apps/hcs2/files/conf/host_controller_service.config").arg(app.applicationDirPath()))};
