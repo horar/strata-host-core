c97d9b687590096e32e39d1c1d4609adbd9d5f01
Lubomir Carik <Lubomir.Carik@onsemi.com>
2019-05-20 16:41:45 +0200

    sgwidgets: re-integrate former simple sgwidgets gallery app
    Not all components was introduced into gallery...

---
+++ host/components/sgwidgets/src/main.cpp
@@ -0,0 +1,51 @@
+#include <QApplication>
+#include <QQmlApplicationEngine>
+
+#include <QDebug>
+#include <QDirIterator>
+#include <QResource>
+
+#include <QLibraryInfo>
+
+int main(int argc, char* argv[])
+{
+    QString resourcePath;
+#ifdef Q_OS_MACOS
+    QDir applicationDir(QCoreApplication::applicationDirPath());
+    applicationDir.cdUp();
+    applicationDir.cdUp();
+    applicationDir.cdUp();
+
+    resourcePath = applicationDir.path();
+#else
+    resourcePath = QCoreApplication::applicationDirPath();
+#endif
+
+    const auto resources = {QString("sgwidgets.rcc"), QString("fonts.rcc")};
+    for (const auto& resourceName : resources) {
+        qDebug() << "Loading '" << resourceName << "':"
+                 << QResource::registerResource(
+                        QString("%1/%2").arg(resourcePath).arg(resourceName));
+    }
+
+    qDebug() << "Source tree:";
+    QDirIterator it(":", QDirIterator::Subdirectories);
+    while (it.hasNext()) {
+        qDebug() << it.next();
+    }
+
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    // https://stackoverflow.com/questions/34099236/qtquick-chartview-qml-object-seg-faults-causes-qml-engine-segfault-during-load
+    QApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.addImportPath(QStringLiteral("qrc:/"));
+
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty()) {
+        return -1;
+    }
+
+    return app.exec();
+}
