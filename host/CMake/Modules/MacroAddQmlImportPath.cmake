0835ef448ea8caad2ce78e0dd7d87dd91146cfb5
Lubomir Carik <Lubomir.Carik@onsemi.com>
2019-06-17 16:56:33 +0200

    cmake: simplify qml import path manipulation

--- host/CMake/Modules/MacroAddQmlImportPath.cmake
+++ host/CMake/Modules/MacroAddQmlImportPath.cmake
@@ -2,15 +2,12 @@ macro(add_qml_import_path)
     set(qmlImportPath PATH)
     cmake_parse_arguments(local "" "${qmlImportPath}" "" ${ARGN})
 
-    message(STATUS "...updating QML import path with with: '${local_PATH}'")
-
-    # [LC] update QML mobule import paths for this project here
     set(QML_DIRS "${QML_IMPORT_PATH}")
-    list(APPEND QML_DIRS "${local_PATH}")
-    list(REMOVE_DUPLICATES "${QML_DIRS}")
-
-    # Additional import path used to resolve QML modules in Qt Creator's code model
-    set(QML_IMPORT_PATH "${QML_DIRS}"
-        CACHE STRING "Qt Creator extra qml import paths" FORCE
-    )
+    if(NOT ${local_PATH} IN_LIST QML_DIRS)
+        # Additional import path used to resolve QML modules in Qt Creator's code model
+        set(QML_IMPORT_PATH "${local_PATH};${QML_IMPORT_PATH}"
+            CACHE STRING "Qt Creator extra qml import paths" FORCE
+        )
+        message(STATUS "... updated QML import path with: '${local_PATH}'")
+    endif()
 endmacro()
