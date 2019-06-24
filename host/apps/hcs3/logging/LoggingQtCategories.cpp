9b95370095801b649ac576930ee4cff56cef4eac
Milan Franc <milan.franc@onsemi.com>
2019-06-24 09:58:52 +0200

    added comment on other categories.

--- host/apps/hcs3/logging/LoggingQtCategories.cpp
+++ host/apps/hcs3/logging/LoggingQtCategories.cpp
@@ -3,3 +3,8 @@
 Q_LOGGING_CATEGORY(logCategoryHcs, "strata.hcs")
 Q_LOGGING_CATEGORY(logCategoryHcsDownloader, "strata.hcs.downloader")
 Q_LOGGING_CATEGORY(logCategoryHcsStorage, "strata.hcs.storage")
+
+//there are also categories:
+// "strata.hcs.database"
+// "strata.hcs.boards"
+
