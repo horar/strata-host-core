d3a4a1209fcafd0d7c7c27abcbe946bc60721b9c
Martin Michalek <martin.michalek@onsemi.com>
2019-06-25 12:31:19 +0200

    SCI: switched to ProgramDeviceWizard from Common

--- host/apps/SerialConsoleInterface/SciModel.cpp
+++ host/apps/SerialConsoleInterface/SciModel.cpp
@@ -1,44 +1,17 @@
 #include "SciModel.h"
 #include "logging/LoggingQtCategories.h"
-#include <PlatformConnection.h>
 
 SciModel::SciModel(QObject *parent)
     : QObject(parent)
 {
     boardController_.initialize();
-
-    connect(&flasherConnector_, &FlasherConnector::taskDone,
-            this, &SciModel::programDeviceDoneHandler);
-
-    connect(&flasherConnector_, &FlasherConnector::notify,
-            this, &SciModel::notify);
 }
 
 SciModel::~SciModel()
 {
 }
 
-void SciModel::programDevice(const QString &connectionId, const QString &firmwarePath)
-{
-    qCInfo(logCategorySci) << connectionId << firmwarePath;
-
-    spyglass::PlatformConnectionShPtr connection = boardController_.getConnection(connectionId);
-    if (connection == nullptr) {
-        qCWarning(logCategorySci) << "unknown connection id" << connectionId;
-        notify(connectionId, "Connection Id not valid.");
-        programDeviceDone(connectionId, false);
-        return;
-    }
-
-    flasherConnector_.start(connection, firmwarePath);
-}
-
 BoardsController *SciModel::boardController()
 {
     return &boardController_;
-}
-
-void SciModel::programDeviceDoneHandler(const QString& connectionId, bool status)
-{
-    emit programDeviceDone(connectionId, status);
 }
