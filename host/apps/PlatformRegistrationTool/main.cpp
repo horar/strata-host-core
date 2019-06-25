97793e3494a70f9c42781342805bf1af047fb106
Martin Michalek <martin.michalek@onsemi.com>
2019-06-25 15:40:56 +0200

    PRT: used ProgramDeviceWizard and BoardsController

--- host/apps/PlatformRegistrationTool/main.cpp
+++ host/apps/PlatformRegistrationTool/main.cpp
@@ -1,13 +1,58 @@
-#include <QDir>
-#include <QFile>
+#include "PrtModel.h"
+#include <BoardsController.h>
+
 #include <QGuiApplication>
-#include <QProcess>
 #include <QQmlApplicationEngine>
 #include <QtQml/QQmlContext>
+#include <QSettings>
+#include <QResource>
+#include <QDir>
 
-#include "SgJLinkConnector.h"
-#include "PrtModel.h"
-#include "SgSortFilterProxyModel.h"
+#include <QtLoggerSetup.h>
+#include "logging/LoggingQtCategories.h"
+
+void loadResources() {
+    QDir applicationDir(QCoreApplication::applicationDirPath());
+
+    const auto resources = {
+        QStringLiteral("component-fonts.rcc"),
+        QStringLiteral("component-common.rcc"),
+        QStringLiteral("component-sgwidgets.rcc")};
+
+#ifdef Q_OS_MACOS
+    applicationDir.cdUp();
+    applicationDir.cdUp();
+    applicationDir.cdUp();
+#endif
+
+    for (const auto& resourceName : resources) {
+        QString resourcePath = applicationDir.filePath(resourceName);
+
+        qCInfo(logCategoryPrt)
+                << "Loading"
+                << resourceName << ":"
+                << QResource::registerResource(resourcePath);
+    }
+}
+
+void addImportPaths(QQmlApplicationEngine *engine) {
+    QDir applicationDir(QCoreApplication::applicationDirPath());
+
+#ifdef Q_OS_MACOS
+    applicationDir.cdUp();
+    applicationDir.cdUp();
+    applicationDir.cdUp();
+#endif
+
+    bool status = applicationDir.cd("imports");
+    if (status == false) {
+        qCCritical(logCategoryPrt) << "failed to find import path.";
+    }
+
+    engine->addImportPath(applicationDir.path());
+
+    engine->addImportPath("qrc:///");
+}
 
 int main(int argc, char *argv[])
 {
@@ -20,11 +65,14 @@ int main(int argc, char *argv[])
     const QtLoggerSetup loggerInitialization(app);
     qCInfo(logCategoryPrt) << QStringLiteral("%1 v%2").arg(QCoreApplication::applicationName()).arg(QCoreApplication::applicationVersion());
 
+    loadResources();
+
     QQmlApplicationEngine engine;
 
+    addImportPaths(&engine);
+
     qmlRegisterType<PrtModel>("tech.strata.prt", 1, 0, "PrtModel");
-    qmlRegisterType<SgJLinkConnector>("tech.strata.prt", 1, 0, "SgJLinkConnector");
-    qmlRegisterType<SgSortFilterProxyModel>("tech.strata.prt", 1, 0, "SgSortFilterProxyModel");
+    qmlRegisterUncreatableType<BoardsController>("tech.strata.sci", 1, 0, "BoardsController", "can not instantiate BoardsController in qml");
 
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty()) {
--- host/apps/PlatformRegistrationTool/main.qml
+++ host/apps/PlatformRegistrationTool/main.qml
@@ -3,46 +3,24 @@ import QtQuick.Window 2.12
 import QtQuick.Controls 2.12
 
 import tech.strata.prt 1.0 as PrtCommon
-import "./common" as Common
+import tech.strata.common 1.0 as Common
 
 Window {
     id: window
     width: 800
-    height: 900
+    height: 600
     minimumWidth: 800
-    minimumHeight: 900
+    minimumHeight: 600
 
     visible: true
     title: qsTr("ON Semiconductor: Platform Registration Tool")
-
-    Rectangle {
-        id: bg
-        anchors.fill: parent
-        color: "#eeeeee"
-    }
-
-    Component.onCompleted: {
-        stackView.pushPage("MainPage.qml")
-    }
 
     PrtCommon.PrtModel {
         id: prtModel
     }
 
-    StackView {
-        id: stackView
-        anchors {
-            fill:parent
-        }
-
-        function pushPage(item, properties, operation) {
-            if (properties === undefined) {
-                properties = {}
-            }
-
-            properties["prtModel"] = prtModel
-
-            push(item, properties, operation)
-        }
+    Common.ProgramDeviceWizard {
+        anchors.fill: parent
+        boardController: prtModel.boardController
     }
 }
