017d0d582d2a9e55c762d292eeecc029c68e6aa0
Lubomir Carik <Lubomir.Carik@onsemi.com>
2019-07-19 12:24:27 +0200

    FIX: loading FontGallery resource path from correct location
    Now it should work in macOS/Windows for standard project build.

--- host/components/fonts/src/main.cpp
+++ host/components/fonts/src/main.cpp
@@ -2,16 +2,38 @@
 #include <QQmlApplicationEngine>
 
 #include <QDebug>
+#include <QDirIterator>
 #include <QResource>
 
 int main(int argc, char* argv[])
 {
-    qDebug() << "LOAD:"
-             << QResource::registerResource(QStringLiteral("../../../component-fonts.rcc"));
-
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
     QGuiApplication app(argc, argv);
+
+    QString resourcePath;
+#ifdef Q_OS_MACOS
+    QDir applicationDir(QCoreApplication::applicationDirPath());
+    applicationDir.cdUp();
+    applicationDir.cdUp();
+    applicationDir.cdUp();
+
+    resourcePath = applicationDir.path();
+#else
+    resourcePath = QCoreApplication::applicationDirPath();
+#endif
+
+    const auto resources = {QStringLiteral("component-fonts.rcc")};
+    for (const auto& resourceName : resources) {
+        qDebug() << "Loading '" << resourceName << "':"
+                 << QResource::registerResource(
+                        QString("%1/%2").arg(resourcePath).arg(resourceName));
+    }
+
+    qDebug() << "Source tree:";
+    QDirIterator it(":", QDirIterator::Subdirectories);
+    while (it.hasNext()) {
+        qDebug() << it.next();
+    }
 
     QQmlApplicationEngine engine;
     engine.addImportPath(QStringLiteral("qrc:/"));
