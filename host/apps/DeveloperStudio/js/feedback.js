ca5fe4df6f2ee3e1551bc147c9de7ac196c45ed5
Luay Alshawi <luay.alshawi@onsemi.com>
2019-10-04 11:41:50 -0700

    Removes feedback data from the log since it contains user email address

--- host/apps/DeveloperStudio/js/feedback.js
+++ host/apps/DeveloperStudio/js/feedback.js
@@ -7,6 +7,5 @@
 */
 function feedbackInfo(feedback_info, success, error){
     var data = {"email": feedback_info.email, "name": feedback_info.name, "company": feedback_info.company, "comment" : feedback_info.comment };
-    console.log(LoggerModule.Logger.devStudioFeedbackCategory, "Feedback body:", JSON.stringify(data));
     Rest.xhr("post", "feedbacks", data, success, error, null);
 }
