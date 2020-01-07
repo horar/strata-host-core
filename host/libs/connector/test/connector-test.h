4b5cd3977e637a93e01159f17bf78450f55d0925
Lubomir Carik <Lubomir.Carik@onsemi.com>
2020-01-07 14:50:00 +0100

    connector: reorganise tests source code & CMake's CTest support

---
+++ host/libs/connector/test/connector-test.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "Connector.h"
+
+#include <string>
+#include <gtest/gtest.h>
+
+
+class ConnectorTest : public testing::Test
+{
+public:
+
+    bool nonBlockingReadPolling(std::unique_ptr<Connector> &connector, std::string &message);
+
+    void dealerMain(const std::string& identity, ReadMode read_mode);
+
+    void routerMain(ReadMode read_mode);
+
+    void subscriberMain(const std::string& identity, ReadMode read_mode);
+
+    void subscriberEmptyMain(const int32_t identities, ReadMode read_mode);
+
+    void publisherMain(const std::vector<std::string>& identities);
+
+    void responseMain(ReadMode read_mode);
+
+    void requestMain(ReadMode read_mode);
+
+    void stop();
+
+protected:
+    void SetUp() override;
+
+    virtual void TearDown() override;
+
+    const std::string ADDRESS{"tcp://127.0.0.1:5555"};
+
+    const uint32_t MAX_IDS{10};
+    const uint32_t LOOP_COUNTER{20};
+
+    const std::vector<std::string> REQUEST_MESSAGE_REFS{"req1", "req2", "req3"};
+    const std::vector<std::string> RESPONSE_MESSAGE_REFS{"resp1", "resp2", "resp3"};
+    const std::vector<std::string> PUBLISHER_MESSAGE_REFS{"pub-sub1", "pub-sub2", "pub-sub3"};
+
+    std::atomic_int mStop_{false};
+};
