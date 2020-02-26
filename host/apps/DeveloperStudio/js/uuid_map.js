cd3649830e4ccdfdf5b6cddc92c15196b4a0c218
taniya_nanda <taniya.nanda@onsemi.com>
2020-02-26 13:23:32 -0800

    removed the debug comment in uuid

--- host/apps/DeveloperStudio/js/uuid_map.js
+++ host/apps/DeveloperStudio/js/uuid_map.js
@@ -43,7 +43,7 @@ var uuid_map = {
     "210": "XDFN-LDO",
     "211": "XDFN-LDO",
     "212": "XDFN-LDO",
-    "213": "sensor",             // empty listing for sensors platform, hack for proper display in static platform list
+    "213": "sensor",
     "214": "XDFN-LDO",
     "215": "5A-switcher",
     "216": "5A-switcher-NCV6357",
