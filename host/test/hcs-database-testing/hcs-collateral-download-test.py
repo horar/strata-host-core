f5371ad9c398a69d07a88e48195e79382582d486
victorbergeronsemi <victorluiz.bergerpereiradasilva@onsemi.com>
2020-03-13 13:55:34 -0700

    Finish main logic for collateral download testing

--- host/test/hcs-database-testing/hcs-collateral-download-test.py
+++ host/test/hcs-database-testing/hcs-collateral-download-test.py
@@ -2,21 +2,31 @@
 #
 # Python 3
 
-import sys, json, os.path, hashlib
+import sys, json, os.path, hashlib, time
 try:
     import zmq
 except ImportError:
     print("\nZeroMQ library for Python is required, visit https://zeromq.org/languages/python/ for instructions.\nExiting.\n")
     sys.exit(-1)
-if len(sys.argv) < 2:
-    print("\nError: no argument provided.\nInvoke as:\n'python hcs-collateral-download-test.py <HCS ENDPOINT>'.")
-    print("Example:\n'python hcs-collateral-download-test.py tcp://127.0.0.1:5563'.\nExiting.\n")
+if len(sys.argv) != 3:
+    print("\nError: incorrect number of arguments provided.\nInvoke from Powershell script 'hcs-collateral-download-testing.ps1',")
+    print("or directly as:\n'python hcs-collateral-download-test.py <APPDATA\HCS DIRECTORY> <HCS ENDPOINT>'.")
+    print("Example:\n'python hcs-collateral-download-test.py C:\\Users\\<USER>\\AppData\\Roaming\\ON Semiconductor\\hcs tcp://127.0.0.1:5563'.\nExiting.\n")
     sys.exit(-1)
-hcs_endpoint = sys.argv[1]
+
+def flushSocketReceive():
+    "Flush/clear this script's end of the socket"
+    while True:
+        try:
+            socket.recv()
+        except:
+            break
 
 def sendToHcsAndWait(message_to_HCS):
     "Send notification to HCS through ZMQ and wait for response"
     # This function will quit the script if it fails
+    # Response is converted to JSON object
+    flushSocketReceive()
     socket.send_string(message_to_HCS)
     try:
         message_from_HCS = socket.recv()
@@ -24,9 +34,36 @@ def sendToHcsAndWait(message_to_HCS):
         print("\nNo response received, is HCS running?\nExiting.\n")
         sys.exit(-1)
     if not message_from_HCS:
-        print("\nError: received empty response, exiting.")
+        print("\nError: received empty or invalid response from HCS, exiting.")
         sys.exit(-1)
-    return json.loads(message_from_HCS)
+    try:
+        message_from_HCS = json.loads(message_from_HCS)
+    except ValueError:
+        print("\nError: received empty or invalid response from HCS, exiting.")
+        sys.exit(-1)
+    return message_from_HCS
+
+def generateDownloadFilesCommand(class_id, download_list):
+    "Compose the hcs::cmd download_files JSON command"
+    cmd = {}
+    cmd['hcs::cmd'] = "download_files"
+    payload = {}
+    payload['destination_dir'] = os.path.join(hcs_directory, "documents", "views", class_id)
+    files = []
+    for file in download_list:
+        files.append(file["uri"])
+    payload['files'] = files
+    cmd['payload'] = payload
+    return json.dumps(cmd)
+
+def getFileMD5Hash(file_abspath):
+    "Calculate the MD5 hash of the given file"
+    # Assumes given file exists (previously checked)
+    with open(filepath, 'rb') as file_md5_check:
+        return hashlib.md5(file_md5_check.read()).hexdigest()
+
+hcs_directory = sys.argv[1]
+hcs_endpoint = sys.argv[2]
 
 # Open socket and connect it to HCS
 context = zmq.Context()
@@ -57,30 +94,34 @@ for platform in platform_list:
     message_from_HCS = sendToHcsAndWait(message_to_HCS)
     try:
         file_list = message_from_HCS["cloud::notification"]["documents"]
+        view_list = [file for file in file_list if file["category"] == "view"]
+        download_list = [file for file in file_list if file["category"] == "download"]
     except KeyError:
-        print("\nError: received empty response from HCS, exiting.")
+        print("\nError: received empty or invalid response from HCS.\nLast response from HCS:\n\n" + json.dumps(message_from_HCS, indent=4) + "\n\nExiting.")
         sys.exit(-1)
-    print(", received reply with " + str(len(file_list)) + " files to be automatically downloaded.")
+    print(", received reply with " + str(len(file_list)) + " files to be automatically downloaded (" +
+        str(len(view_list)) + " views, " + str(len(download_list)) + " downloads).\n\nDownloading files and verifying...\n")
+    js = generateDownloadFilesCommand(str(platform["class_id"]), download_list)
+    socket.send_string(js)
+    time.sleep(len(download_list))
     for file in file_list:
-        if os.path.isfile(file["uri"]): # File found where expected - perform MD5 check
-            with open(file["uri"], 'rb') as file_md5_check:
+        filepath = file["uri"] if os.path.isabs(file["uri"]) else os.path.join(hcs_directory, "documents", "views", str(platform["class_id"]), file["prettyname"])
+        if os.path.isfile(filepath): # File found where expected - perform MD5 check
+            with open(filepath, 'rb') as file_md5_check:
                 calculated_md5 = hashlib.md5(file_md5_check.read()).hexdigest()
                 if calculated_md5 != file["md5"]: # MD5 check failed
-                    print("\nTest failed, MD5 check unsuccessful for file:\n" + file["uri"])
+                    print("\nTest failed, MD5 check unsuccessful for file:\n" + filepath)
                     platform_failed_tests += 1
                     total_failed_tests += 1
         else: # File not found where expected
-            print("\nTest failed, file not found:\n" + file["uri"])
+            print("\nTest failed, file not found:\n" + filepath)
             platform_failed_tests += 1
             total_failed_tests += 1
     if platform_failed_tests < 1:
         print("\nAll tests PASSED for platform " + str(platform["class_id"]) + ".")
     else:
         print("\nWARNING:\nA total of " + str(platform_failed_tests) + " unsuccessful test cases were found for this platform.")
-
-
-    break
-        # print("\nChecking for file " + file["uri"])
-        # print("OK, FILE FOUND") if os.path.isfile(file["uri"]) else print("FAILED, FILE NOT FOUND")
-
-print("\n\npython done -- total failed tests " + str(total_failed_tests))
+if total_failed_tests < 1:
+    print("\nAll tests PASSED for all platforms.\n")
+else:
+    print("\nWARNING:\nA total of " + str(total_failed_tests) + " unsuccessful test cases were found between all platforms.")
--- host/test/hcs-database-testing/hcs-collateral-download-testing.ps1
+++ host/test/hcs-database-testing/hcs-collateral-download-testing.ps1
@@ -5,6 +5,13 @@
 # Define HCS TCP endpoint to be used
 Set-Variable "HCS_TCP_endpoint" "tcp://127.0.0.1:5563"
 
+# Define path to AppData/Roaming/ON Semiconductor
+Set-Variable "AppData_HCS_dir" "$Env:AppData\ON Semiconductor\hcs"
+
+#####
+##### Automated section
+#####
+
 # Search for Python in local host machine
 Try {
     $python_ver = python -V
@@ -18,13 +25,19 @@ Try {
     Exit
 }
 
-#  Search for Python script
+# Search for Python script
 If (!(Test-Path hcs-collateral-download-test.py)) {
     ""; 'Error: Python script "hcs-collateral-download-test.py" not found in current directory.'; "";
     Exit
 }
 
+# Search for "hcs" directory
+If (!(Test-Path $AppData_HCS_dir -PathType Any)) {
+    ""; "Error: Directory $AppData_HCS_dir not found."; "";
+    Exit
+}
+
 ""; "Running Python script...";
-python hcs-collateral-download-test.py $HCS_TCP_endpoint
+python hcs-collateral-download-test.py $AppData_HCS_dir $HCS_TCP_endpoint
 
 ""; "Testing complete."; ""; "";
