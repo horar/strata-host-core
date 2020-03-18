3ad37d080fc90dee39d4cd38d9cb64b220059f0f
Mohammed Algadhib <mohammed.algadhib@onsemi.com>
2020-03-17 19:16:17 -0700

    more modular approach to create the test scripts, fixed some formatting and typos

--- host/test/release-testing/strataDev/Test-SDSControlViews.ps1
+++ host/test/release-testing/strataDev/Test-SDSControlViews.ps1
@@ -1,98 +1,41 @@
-# This test is semi automated as it requires someone to look at the UI for proper rendering
+<#
+.SYNOPSIS
+Modular file that export Test-StrataControlView function to test the cycle through the control views
+of Strata Developer Studio.
 
-# squence of test:
-#   1. Run Strata Dev Studio.
-#   2. Start the python script `zmq-client.py`. 
-#   3. Close every thing.
+.DESCRIPTION
+Modular file that export Test-StrataControlView function to test the cycle through the control views
+of Strata Developer Studio. This is part of the automated test script for the master test plan
+https://ons-sec.atlassian.net/wiki/spaces/SPYG/pages/775848204/Master+test+plan+checklist#Installer
 
-# Optional -StrataPath command line argument, otherwise use the default intallation path.
-[CmdletBinding()]
-param (
-    [string]$StrataPath = "$($Env:Programfiles)\ON Semiconductor\Strata Developer Studio\Strata Developer Studio.exe",
-    [string]$PythonScriptPath = "$($PSScriptRoot)\control-view-test.py"
-)
+.INPUTS
+-PythonScriptPath   <Path to Strata executable>
+-StrataPath         <Path to control-view-test.py>
 
-# Determine the python command based on OS. osx will execute python 2 by default and here we need to use python3.
-# on win, python3 is not in the path by default, as a result we'll need to use python3 for osx and python for win
-# If ($IsWindows -eq $true) {
-If ($Env:OS -eq "Windows_NT") {
-    $PythonExec = 'python'
-}
-Else {
-    $PythonExec = 'python3'
-}
+.OUTPUTS
+Bolean result of the test
 
-# Utility function to print the exit code an a pattern for the end of the script.
-function Exit-TestScript {
+.NOTES
+Version:        1.0
+Creation Date:  03/17/2020
+#>
+
+# Function to run the test, it will return True if the test was successful
+# Usage: Test-StrataControlView -PythonScriptPath <Path to Strata executable> -StrataPath <Path to control-view-test.py>
+function Test-StrataControlView {
+    [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true)][int]$ScriptExitCode
+        [Parameter(Mandatory = $true)][string]$PythonScriptPath,    # Path to Strata executable
+        [Parameter(Mandatory = $true)][string]$StrataPath           # Path to control-view-test.py
     )
-    if ($ScriptExitCode -eq 0) {
-        write-host "Test finished Successfully. Existting..." -ForegroundColor Green
-    }
-    else {
-        write-host "Test failed. Terminating" $ScriptExitCode -ForegroundColor red
-    }
-    write-host "============================================================================================================================================"
-    write-host "============================================================================================================================================"
-    exit $ScriptExitCode
-}
-
-# function to check if python and pyzmq are installed, if both were found it will return True,
-function Test-PythonExist {
-    Try {
-        Write-Host "Looking for python..."
-        If ((Start-Process $PythonExec --version -Wait -WindowStyle Hidden -PassThru).ExitCode -Eq 0) {
-            Write-Host "Python found." -ForegroundColor Green
-            Write-Host "Looking for pyzmq..."
-            If ((Start-Process $PythonExec '-c "import zmq"' -WindowStyle Hidden -Wait -PassThru).ExitCode -Eq 0) {
-                Write-Host "pyzmq found." -ForegroundColor Green
-            }
-            Else {
-                Write-Host "pyzmq not found. aborting..." -ForegroundColor red
-                Return $false
-            }
-        } 
-        Else {           
-            Write-Host "Python not found. aborting..." -ForegroundColor red
-            Return $false
-        }  
-    }
-    Catch [System.InvalidOperationException] {
-        Write-Host "Python not found. aborting..." -ForegroundColor red
-        Return $false
-    }
-    Return $true
-}
-
-# This functin is to check if Strata Develoiper studio Exist
-function Test-StrataExist {
-
-    # Convert the path if using unix env.
-    If ( $Env:OS -ne "Windows_NT" -and (( $StrataPath = Convert-path $StrataPath) -eq $false )) {
-        return $false
-    }
     
-    Write-Host "Looking for Strata Developer Studio in" $StrataPath
-    If (Test-Path -Path $StrataPath) {
-        Write-Host "Strata Developer Studio found." -ForegroundColor Green
-        Return $true
+    # Determine the python command based on OS. osx will execute python 2 by default and here we need to use python3.
+    # on win, python3 is not in the path by default, as a result we'll need to use python3 for osx and python for win
+    If ($Env:OS -eq "Windows_NT") {
+        $PythonExec = 'python'
     }
     Else {
-        Write-Host "Strta Developer Studio not found. aborting..." -ForegroundColor red
-        Return $false 
-    }
-}
-
-# Function to run the test, it will return True if the test was successful 
-function Test-StrataControlView {
-    # check if the python script exesit.
-    
-    # Convert the path if using unix env.
-    If ( $Env:OS -ne "Windows_NT" -and (( $PythonScriptPath = Convert-path $PythonScriptPath) -eq $false )) {
-        If ( ($PythonScriptPath = Convert-path $PythonScriptPath) -eq $false ) {
-            return $false
-        }
+        $PythonExec = 'python3'
     }
 
     Write-Host "Looking for the test script " $PythonScriptPath
@@ -103,9 +46,9 @@ function Test-StrataControlView {
         ($strataDev = Start-Process $StrataPath -PassThru) | Out-Null     # Hide output.
         
         write-host "Starting python test script..."
-        write-host "############################################################################################################################################"
+        write-host "################################################################################"
         $pythonScript = Start-Process $PythonExec $PythonScriptPath -NoNewWindow -PassThru -wait
-        write-host "############################################################################################################################################"
+        write-host "################################################################################"
         
         Write-Host "Python test script is done."
 
@@ -115,7 +58,7 @@ function Test-StrataControlView {
             stop-process $strataDev.id
         }
         else {
-            # Strta is not running. it could bea crash!
+            # Strata is not running. it could be crash!
             Write-Host "Strata developer Studio is not running. It might crashed during the test. Aborting..." -ForegroundColor Yellow
             return $false
         }
@@ -136,21 +79,3 @@ function Test-StrataControlView {
         return $false
     }
 }
-
-# check python tools.
-if ( (Test-PythonExist) -eq $false ) {
-    Exit-TestScript -ScriptExitCode -1
-}
-
-# verify Strata path.
-if ( (Test-StrataExist) -Eq $false) {
-    Exit-TestScript -ScriptExitCode -1
-}
-
-# verify Strata path.
-if ( (Test-StrataControlView) -Eq $false) {
-    Exit-TestScript -ScriptExitCode -1
-}
-
-# Test successfull!
-Exit-TestScript -ScriptExitCode 0
