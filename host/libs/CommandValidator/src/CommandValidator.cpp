6d3cccaee3f0aece909e11f7c3a9560f324853ca
Mohammed Algadhib <mohammed.algadhib@onsemi.com>
2020-04-09 10:13:18 -0700

    Logic gates board is not validated because of an extra verbose_name property in the payload. updated the schema and the test.

--- host/libs/CommandValidator/src/CommandValidator.cpp
+++ host/libs/CommandValidator/src/CommandValidator.cpp
@@ -24,7 +24,7 @@ const rapidjson::SchemaDocument CommandValidator::requestPlatformIdResSchema(
                     "pattern":"^platform_id$"
                     },
                     "payload": {
-                        "oneOf":[
+                        "anyOf":[
                             {
                                 "type": "object",
                                 "properties": {
