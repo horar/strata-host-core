82727ac0244527c58060496d16d5f5fa4ab68719
Tyler Sharp <tyler.sharp@onsemi.com>
2020-05-05 14:34:30 -0700

    added class ids from dp to uuid_map

--- host/apps/DeveloperStudio/js/uuid_map.js
+++ host/apps/DeveloperStudio/js/uuid_map.js
@@ -79,5 +79,7 @@ var uuid_map = {
         Real UUID generated from interaction with Deployment Portal (class_id)
     *****/
     "72ddcc10-2d18-4316-8170-5223162e54cf": "sensor",
-    "87054646-955d-42ed-aa82-8927b6a70286": "motorController"
+    "87054646-955d-42ed-aa82-8927b6a70286": "motorController",
+    "b8a53467-4155-4104-905e-8d23bb5664a3": "Adj-LDO",
+    "8da4158e-caa7-469d-8e4c-949d7a7e9858": "Adj-LDO"
 }
