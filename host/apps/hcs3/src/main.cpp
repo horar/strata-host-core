d3d1b07cbde3e9fe25e27780a5e37939361e7811
Lubomir Carik <Lubomir.Carik@onsemi.com>
2020-05-15 17:11:07 +0200

    HCS: command line option to clear service local data
    It will remove 'db' and 'documents' folder only when service is not running.

--- host/apps/hcs3/src/main.cpp
+++ host/apps/hcs3/src/main.cpp
@@ -2,7 +2,6 @@
 
 #include "HostControllerServiceVersion.h"
 #include "HostControllerServiceTimestamp.h"
-
 #include "RunGuard.h"
 
 #include "logging/LoggingQtCategories.h"
@@ -12,6 +11,8 @@
 #include <QCoreApplication>
 #include <QCommandLineParser>
 #include <QSettings>
+#include <QStandardPaths>
+#include <QDir>
 
 #include <EvEventsMgr.h>    //for EvEventsMgrInstance (windows WSA)
 
@@ -29,14 +30,40 @@ int main(int argc, char *argv[])
     parser.setApplicationDescription("Strata Host Controller Service");
     parser.addOption({
         {QStringLiteral("f")},
-         QObject::tr("Optional configuration <filename> (default: AppConfigLocation)."),
-         QObject::tr("filename")
+        QObject::tr("Optional configuration <filename> (default: AppConfigLocation)."),
+        QObject::tr("filename")
+    });
+    parser.addOption({
+        {QStringLiteral("c")},
+        QObject::tr("Clear cache data of Host Controller Service.")
     });
     parser.addVersionOption();
     parser.addHelpOption();
     parser.process(app);
 
     RunGuard appGuard{"tech.strata.hcs"};
+
+    if (parser.isSet(QStringLiteral("c"))) {
+        if (appGuard.tryToRun() == false) {
+            qCritical() << QStringLiteral("Host Controller Service is already running - can't clear the cache data!!");
+            return EXIT_FAILURE;
+        }
+
+        const QString cacheDir{QStandardPaths::writableLocation(QStandardPaths::AppDataLocation)};
+        if (cacheDir.isEmpty()) {
+            qWarning() << "Folder with application cached data either not accessible or not found!!";
+            return EXIT_FAILURE;
+        }
+        qDebug() << "Cache location:" << cacheDir;
+
+        for (const auto folder : {QStringLiteral("db"), QStringLiteral("documents")}) {
+            QDir dir(QString("%1/%2").arg(cacheDir).arg(folder));
+            qInfo() << "Removing" << dir.path() << ":" << dir.removeRecursively();
+        }
+
+        return EXIT_SUCCESS;
+    }
+
     const QtLoggerSetup loggerInitialization(app);
     qCInfo(logCategoryHcs) << QStringLiteral("================================================================================");
     qCInfo(logCategoryHcs) << QStringLiteral("%1 %2").arg(QCoreApplication::applicationName()).arg(QCoreApplication::applicationVersion());
