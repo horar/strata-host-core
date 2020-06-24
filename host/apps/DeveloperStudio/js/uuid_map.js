617e4cae1a63a86254cf7a33ca0698d75478e964
Tyler Sharp <tyler.sharp@onsemi.com>
2020-06-24 14:14:22 -0700

    fixed spacing

--- host/apps/DeveloperStudio/js/uuid_map.js
+++ host/apps/DeveloperStudio/js/uuid_map.js
@@ -82,5 +82,5 @@ var uuid_map = {
     "87054646-955d-42ed-aa82-8927b6a70286": "motorController",
     "b8a53467-4155-4104-905e-8d23bb5664a3": "Adj-LDO",
     "8da4158e-caa7-469d-8e4c-949d7a7e9858": "Adj-LDO",
-	"d0cc3eb2-f822-4955-afa4-b975957aed38": "Adj-LDO"
+    "d0cc3eb2-f822-4955-afa4-b975957aed38": "Adj-LDO"
 }
