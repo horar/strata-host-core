09047be16444e2123195825f377997b73bdc8ec3
Lubomir Carik <Lubomir.Carik@onsemi.com>
2020-07-03 19:28:31 +0200

    connector: log 0MQ API version & log-formatting cleanup

--- host/libs/connector/src/ZmqConnector.cpp
+++ host/libs/connector/src/ZmqConnector.cpp
@@ -7,6 +7,11 @@
 ZmqConnector::ZmqConnector(int type)
     : Connector(), context_(new zmq::context_t()), socket_(new zmq::socket_t(*context_, type))
 {
+    int major{0};
+    int minor{0};
+    int patch{0};
+    zmq_version(&major, &minor, &patch);
+    CONNECTOR_DEBUG_LOG("0MQ API version: %d.%d.%d\n", major, minor, patch);
 }
 
 ZmqConnector::~ZmqConnector()
@@ -65,7 +70,7 @@ bool ZmqConnector::read(std::string& message, ReadMode read_mode)
             }
             break;
         default:
-            CONNECTOR_DEBUG_LOG("[Socket] read failed\n",);
+            CONNECTOR_DEBUG_LOG("%s", "[Socket] read failed\n");
             break;           
     }
     return false;
