40cd6ad3df3a00c23dce2cf9cb9a3e9457acb6d5
Lubomir Carik <Lubomir.Carik@onsemi.com>
2020-08-05 14:48:58 +0200

    logger: 'couchbase-lite' category allow filtering like other Qt categories
    Note: only root node allow filtering i.e. filtering sub-categories won't work because
    no categories exists for CB sub-domains.

--- host/libs/loggers/CbLogger/src/CbLoggerSetup.cpp
+++ host/libs/loggers/CbLogger/src/CbLoggerSetup.cpp
@@ -4,31 +4,36 @@
 
 #include <QHash>
 #include <QString>
+
 #include <litecore/c4Base.h>
 
 using std::string;
-
 
 QtMessageHandler g_qtLogCallback = nullptr;
 
 void c4LogCallback(C4LogDomain domain, C4LogLevel level, const char *fmt, va_list args)
 {
-    static const QHash<int, QtMsgType> cb2qtLevels{
-        {kC4LogDebug, QtDebugMsg},
-        {kC4LogVerbose, QtInfoMsg},
-        {kC4LogInfo, QtInfoMsg},
-        {kC4LogWarning, QtWarningMsg},
-        {kC4LogError, QtCriticalMsg}
-    };
-
-    string tag(logCategoryCbLogger);
-    if (const string domainName (c4log_getDomainName(domain)); domainName.empty() == false) {
-        tag += "." + domainName;
+    if (g_qtLogCallback == nullptr) {
+        return;
     }
 
-    const QString msg = QString::vasprintf(fmt, args);
-    QMessageLogContext context{nullptr, 0, "n/a", tag.c_str()};
-    g_qtLogCallback(cb2qtLevels[level], context, msg);
+    static const QHash<int, QtMsgType> cb2qtLevels{{kC4LogDebug, QtDebugMsg},
+                                                   {kC4LogVerbose, QtInfoMsg},
+                                                   {kC4LogInfo, QtInfoMsg},
+                                                   {kC4LogWarning, QtWarningMsg},
+                                                   {kC4LogError, QtCriticalMsg}};
+
+    if (const auto msgType{cb2qtLevels[level]}; logCategoryCbLogger().isEnabled(msgType)) {
+        string tag(logCategoryCbLoggerName);
+        if (const string domainName(c4log_getDomainName(domain)); domainName.empty() == false) {
+            tag += "." + domainName;
+        }
+
+        const QString msg{QString::vasprintf(fmt, args)};
+        const QMessageLogContext context{nullptr, 0, "n/a", tag.c_str()};
+
+        g_qtLogCallback(msgType, context, msg);
+    }
 }
 
 void cbLoggerSetup(QtMessageHandler qtLogCallback)
