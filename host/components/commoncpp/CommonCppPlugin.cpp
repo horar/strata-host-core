b80e8f590b9c25821c14aa4056da49113424eb00
Carter Olsen <carter.olsen@onsemi.com>
2020-08-27 21:19:56 +0000

    Merged in CS-910-harden-version-checking-in-sds (pull request #1324)
    SDS: Added a version checking library
    
    * SDS Version Util: Made SGVersionUtils a singleton
    
    * SDS Version Checking: Adjusted the way that we compare versions.
    
    Now we compare only the <major>.<minor>.<patch> between strings.
    
    * SDS Version Util: Now using the QVersionNumber class to do comparisons
    
    * SDS Version Utility: Added more detail to the compare function description
    
    * SDS: Refactored common components so we can create unit tests
    
    * SDS: Added ability to create unit test for common cpp components
    
    * SDS Version Utility: Fixed logic that was making compare function not return the right result
    
    * Merged develop into CS-910-harden-version-checking-in-sds
    
    * SDS Common Cpp: Removed unnecessary logging categories
    
    * Merge branch 'CS-910-harden-version-checking-in-sds' of https://bitbucket.org/onsemi-spyglass/spyglass into CS-910-harden-version-checking-in-sds
    
    Approved-by: David Faller
    Approved-by: Luay Alshawi

--- host/components/commoncpp/CommonCppPlugin.cpp
+++ host/components/commoncpp/CommonCppPlugin.cpp
@@ -5,6 +5,7 @@
 #include "SGSortFilterProxyModel.h"
 #include "SGQWTPlot.h"
 #include "SGUserSettings.h"
+#include "SGVersionUtils.h"
 #include "mqtt/SGMqttClient.h"
 #include "mqtt/SGSslConfiguration.h"
 
@@ -27,4 +28,5 @@ void CommonCppPlugin::registerTypes(const char *uri)
     qmlRegisterType<QmlMqttClient>(uri, 1, 0, "SGMqttClient");
     qmlRegisterUncreatableType<QmlMqttSubscription>(uri, 1, 0, "SGMqttSubscription", QLatin1String("Subscriptions are read-only"));
     qmlRegisterType<QmlSslConfiguration>(uri, 1, 0, "SGSslConfiguration");
+    qmlRegisterSingletonType<SGVersionUtils>(uri, 1, 0, "SGVersionUtils", SGVersionUtils::SingletonTypeProvider);
 }
