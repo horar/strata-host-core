58248b2533f5003838e74983d04c5d2eaa5f3a82
victorbergeronsemi <victorluiz.bergerpereiradasilva@onsemi.com>
2020-09-28 16:45:13 -0700

    Refactor curl retries and add docker checks to CB launcher

--- host/research_spikes/ota/demo-app/dcftest/start.sh
+++ host/research_spikes/ota/demo-app/dcftest/start.sh
@@ -3,26 +3,74 @@
 # Starts the Couchbase and a simple file server
 # Adds documents to the Couchbase
 
-res="1"
-while test "$res" != "0";
-do
-    ./scripts/up.sh
-    ./scripts/cb_setup.sh
-    ./scripts/cb_add_docs.sh
+COUCHBASE_ENDPOINT="http://127.0.0.1:8091"
+SYNC_GATEWAY_ENDPOINT="http://127.0.0.1:8093/query/service"
+
+# Check Docker is running
+docker_ps=$(docker ps)
+if test "$?" != "0"; then
+    echo "Error - Docker may not be running, aborting."
+    exit 1
+fi
+
+echo "Bringing up Couchbase containers...\n"
+
+./scripts/up.sh
+couchbase_sg_docker_ps=$(docker-compose ps -q couchbase)
+
+if test "$?" != "0" || test "$couchbase_sg_docker_ps" == ""; then
+    echo "\nError bringing up Couchbase containers, aborting."
+    exit 1
+else
+    echo "\nCouchbase containers up, waiting for services to start (this can take up to 30 seconds)..."
+fi
+
+curl_retries_counter=0
+curl_retries_max=30
+res=""
+while test "$res" != "0" && test "$curl_retries_counter" -lt "$curl_retries_max"; do
+    curl --silent --output /dev/null $COUCHBASE_ENDPOINT
     res=$?
-    clear
-    if test "$res" != "0";
-    then
-        echo "*******************************"
-        echo "* Waiting for CB to start ... *"
-        echo "*******************************"
-        sleep 1
-    else
-        echo
-        echo "*********************"
-        echo "* CB UP and RUNNING *"
-        echo "*********************"
-        echo
-    fi
+    curl_retries_counter=$((curl_retries_counter+1))
+    sleep 1
 done
 
+if test "$res" == "0"; then
+    echo "\nCouchbase services successfully connected!"
+else
+    echo "\nError: Couchbase services not connected, timed out."
+    curl $COUCHBASE_ENDPOINT
+    exit 1
+fi
+
+# Couchbase setup
+echo "\nRunning Couchbase setup... "
+./scripts/cb_setup.sh $COUCHBASE_ENDPOINT
+
+curl_retries_counter=0
+res=""
+while test "$res" != "0" && test "$curl_retries_counter" -lt "$curl_retries_max"; do
+    curl --silent --output /dev/null $SYNC_GATEWAY_ENDPOINT
+    res=$?
+    curl_retries_counter=$((curl_retries_counter+1))
+    sleep 1
+done
+
+if test "$res" == "0"; then
+    echo "\nCouchbase services successfully setup!"
+else
+    echo "\nError: Couchbase setup not successful, timed out."
+    curl $SYNC_GATEWAY_ENDPOINT
+    exit 1
+fi
+
+# Couchbase add docs
+echo "\nRunning Couchbase add docs... "
+./scripts/cb_add_docs.sh $COUCHBASE_ENDPOINT $SYNC_GATEWAY_ENDPOINT
+
+if test "$?" == "0"; then
+    echo "\nCouchbase successfully added docs!"
+else
+    echo "\nError: Couchbase did not successfully add docs."
+    exit 1
+fi
