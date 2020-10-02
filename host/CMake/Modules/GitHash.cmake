158a7f5b1a60676d1e636b00180c39845019e8bf
Mustafa Alshehab <Mustafa.Alshehab@onsemi.com>
2020-10-02 00:54:00 -0700

    complete restructure of 3p dir

--- host/CMake/Modules/GitHash.cmake
+++ host/CMake/Modules/GitHash.cmake
@@ -1,6 +1,12 @@
 macro(get_git_hash GIT_REPO_PATH)
+    if(${ARGC} GREATER 1)
+        set(REF ${ARGV1})
+    else()
+        set(REF "HEAD")
+    endif()
+
     execute_process(
-        COMMAND ${GIT_EXECUTABLE} rev-parse --short=5 HEAD
+        COMMAND ${GIT_EXECUTABLE} rev-parse --short=5 ${REF}
         WORKING_DIRECTORY ${GIT_REPO_PATH}
         OUTPUT_VARIABLE GIT_HASH
         RESULTS_VARIABLE GIT_HASH_RESULT
