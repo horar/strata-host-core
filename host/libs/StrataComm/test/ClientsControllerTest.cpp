40155bb14246caea90efbc2f2d3b23d4582ffaa3
Mohammed Algadhib <mohammed.algadhib@onsemi.com>
2020-11-05 14:27:33 -0800

    cleaned up ClientsController tests.

--- host/libs/StrataComm/test/ClientsControllerTest.cpp
+++ host/libs/StrataComm/test/ClientsControllerTest.cpp
@@ -1,80 +1,84 @@
 #include "ClientsControllerTest.h"
 
-void ClientsControllerTest::testFunction1() {
-    ClientsController cc;
-    cc.registerClient(Client("AA", "v1.0"));
-    cc.registerClient(Client("BB", "v2.0"));
-    cc.registerClient(Client("CC", "v3.0"));
-    cc.registerClient(Client("DD", "v4.0"));
-    cc.registerClient(Client("EE", "v5.0"));
-    cc.registerClient(Client("FF", "v6.0"));
-    cc.registerClient(Client("GG", "v7.0"));
-    cc.registerClient(Client("HH", "v8.0"));
-    cc.registerClient(Client("II", "v9.0"));
-    cc.registerClient(Client("JJ", "v10.1"));
-    cc.registerClient(Client("KK", "v11.0"));
-    cc.registerClient(Client("LL", "v12.0"));
-    cc.registerClient(Client("MM", "v13.0"));
-    cc.registerClient(Client("NN", "v14.3"));
+void ClientsControllerTest::notifyClientMock(const Client &client, const QJsonObject &payload) {
+    qDebug() << "Sending " << payload << "To Client ID: " << client.getClientID() << " API Version: " << client.getApiVersion();
+}
 
-    QCOMPARE_(cc.isRegisteredClient("AA"), true);
-    QCOMPARE_(cc.isRegisteredClient("BB"), true);
-    QCOMPARE_(cc.isRegisteredClient("CC"), true);
-    QCOMPARE_(cc.isRegisteredClient("DD"), true);
-    QCOMPARE_(cc.isRegisteredClient("EE"), true);
-    QCOMPARE_(cc.isRegisteredClient("FF"), true);
-    QCOMPARE_(cc.isRegisteredClient("GG"), true);
-    QCOMPARE_(cc.isRegisteredClient("HH"), true);
-    QCOMPARE_(cc.isRegisteredClient("II"), true);
-    QCOMPARE_(cc.isRegisteredClient("JJ"), true);
-    QCOMPARE_(cc.isRegisteredClient("KK"), true);
-    QCOMPARE_(cc.isRegisteredClient("LL"), true);
-    QCOMPARE_(cc.isRegisteredClient("MM"), true);
-    QCOMPARE_(cc.isRegisteredClient("NN"), true);
+void ClientsControllerTest::testIsRegisteredClient() {
+    ClientsController clientsController;
+    Client client1("client_1", "v1.0.0");
+    Client client2("client_2", "v1.0.0");
 
-    QCOMPARE_(cc.unregisterClient(Client("AA", "v1.0")), true);
-    QCOMPARE_(cc.unregisterClient(Client("BB", "v2.0")), true);
-    QCOMPARE_(cc.unregisterClient(Client("CC", "v3.0")), true);
-    QCOMPARE_(cc.unregisterClient(Client("DD", "v4.0")), true);
-    QCOMPARE_(cc.unregisterClient(Client("EE", "v5.0")), true);
-    QCOMPARE_(cc.unregisterClient(Client("FF", "v6.0")), true);
-    QCOMPARE_(cc.unregisterClient(Client("GG", "v7.0")), true);
-    QCOMPARE_(cc.unregisterClient(Client("HH", "v8.0")), true);
-    QCOMPARE_(cc.unregisterClient(Client("II", "v9.0")), true);
-    QCOMPARE_(cc.unregisterClient(Client("JJ", "v10.1")), true);
-    QCOMPARE_(cc.unregisterClient(Client("KK", "v11.0")), true);
-    QCOMPARE_(cc.unregisterClient(Client("LL", "v12.0")), true);
-    QCOMPARE_(cc.unregisterClient(Client("MM", "v13.0")), true);
-    QCOMPARE_(cc.unregisterClient(Client("NN", "v14.3")), true);
+    QCOMPARE_(clientsController.isRegisteredClient(client1.getClientID()), false);
+    QCOMPARE_(clientsController.isRegisteredClient(client2.getClientID()), false);
 
-    QCOMPARE_(cc.unregisterClient(Client("AA", "v1.0")), false);
-    QCOMPARE_(cc.unregisterClient(Client("BB", "v2.0")), false);
-    QCOMPARE_(cc.unregisterClient(Client("CC", "v3.0")), false);
-    QCOMPARE_(cc.unregisterClient(Client("DD", "v4.0")), false);
-    QCOMPARE_(cc.unregisterClient(Client("EE", "v5.0")), false);
-    QCOMPARE_(cc.unregisterClient(Client("FF", "v6.0")), false);
-    QCOMPARE_(cc.unregisterClient(Client("GG", "v7.0")), false);
-    QCOMPARE_(cc.unregisterClient(Client("HH", "v8.0")), false);
-    QCOMPARE_(cc.unregisterClient(Client("II", "v9.0")), false);
-    QCOMPARE_(cc.unregisterClient(Client("JJ", "v10.1")), false);
-    QCOMPARE_(cc.unregisterClient(Client("KK", "v11.0")), false);
-    QCOMPARE_(cc.unregisterClient(Client("LL", "v12.0")), false);
-    QCOMPARE_(cc.unregisterClient(Client("MM", "v13.0")), false);
-    QCOMPARE_(cc.unregisterClient(Client("NN", "v14.3")), false);
+    QCOMPARE_(clientsController.registerClient(client1), true);
 
-    QCOMPARE_(cc.isRegisteredClient("AA"), false);
-    QCOMPARE_(cc.isRegisteredClient("BB"), false);
-    QCOMPARE_(cc.isRegisteredClient("CC"), false);
-    QCOMPARE_(cc.isRegisteredClient("DD"), false);
-    QCOMPARE_(cc.isRegisteredClient("EE"), false);
-    QCOMPARE_(cc.isRegisteredClient("FF"), false);
-    QCOMPARE_(cc.isRegisteredClient("GG"), false);
-    QCOMPARE_(cc.isRegisteredClient("HH"), false);
-    QCOMPARE_(cc.isRegisteredClient("II"), false);
-    QCOMPARE_(cc.isRegisteredClient("JJ"), false);
-    QCOMPARE_(cc.isRegisteredClient("KK"), false);
-    QCOMPARE_(cc.isRegisteredClient("LL"), false);
-    QCOMPARE_(cc.isRegisteredClient("MM"), false);
-    QCOMPARE_(cc.isRegisteredClient("NN"), false);
+    QCOMPARE_(clientsController.isRegisteredClient(client1.getClientID()), true);
+    QCOMPARE_(clientsController.isRegisteredClient(client2.getClientID()), false);
+}
 
+void ClientsControllerTest::testRegisterClient() {
+    ClientsController clientsController;
+    QList<Client> testClientsList;
+    constexpr int testClientListSize = 100;
+
+    // create a list of random clients and register them.
+    for (int i=0; i<testClientListSize; i++) {
+        testClientsList.append({QByteArray::number(i), "v1.0.0"});
+        QCOMPARE_(clientsController.registerClient(testClientsList[i]), true);
+    }
+
+    for(const auto &client : testClientsList) {
+        QCOMPARE_(clientsController.isRegisteredClient(client.getClientID()), true);
+    }
+}
+
+void ClientsControllerTest::testRegisterDublicateClient() {
+    ClientsController clientsController;
+    Client client("client", "v1.0.0");
+    Client clientDuplicateID("client", "v2.0.0");
+
+    QCOMPARE_(clientsController.isRegisteredClient(client.getClientID()), false);
+    QCOMPARE_(clientsController.isRegisteredClient(clientDuplicateID.getClientID()), false);
+
+    QCOMPARE_(clientsController.registerClient(client), true);
+    QCOMPARE_(clientsController.registerClient(clientDuplicateID), false);
+}
+
+void ClientsControllerTest::testUnregisterClient() {
+    ClientsController clientsController;
+    Client client1("client_1", "v1.0.0");
+    Client client2("client_2", "v1.0.0");
+
+    QCOMPARE_(clientsController.registerClient(client1), true);
+    QCOMPARE_(clientsController.registerClient(client2), true);
+
+    QCOMPARE_(clientsController.isRegisteredClient(client1.getClientID()), true);
+    QCOMPARE_(clientsController.isRegisteredClient(client2.getClientID()), true);
+
+    QCOMPARE_(clientsController.unregisterClient(client1.getClientID()), true);
+    QCOMPARE_(clientsController.unregisterClient(client2.getClientID()), true);
+
+    QCOMPARE_(clientsController.unregisterClient(client1.getClientID()), false);
+    QCOMPARE_(clientsController.unregisterClient(client2.getClientID()), false);
+}
+
+void ClientsControllerTest::testNotifyAllCleints() {
+    ClientsController clientsController;
+    QSignalSpy signalSpy(&clientsController, &ClientsController::notifyClientSignal);
+    
+    connect(&clientsController, &ClientsController::notifyClientSignal, this, &ClientsControllerTest::notifyClientMock);
+
+    clientsController.registerClient(Client("AA", "v1.0"));
+    clientsController.registerClient(Client("BB", "v2.0"));
+    clientsController.registerClient(Client("CC", "v3.0"));
+    clientsController.registerClient(Client("DD", "v4.0"));
+    clientsController.registerClient(Client("EE", "v5.0"));
+
+    clientsController.notifyAllClients(QJsonObject({{"key", 1}}));
+
+    QCOMPARE_(signalSpy.count(), 5);
+    
+    disconnect(&clientsController, &ClientsController::notifyClientSignal, this, &ClientsControllerTest::notifyClientMock);
 }
--- host/libs/StrataComm/test/ClientsControllerTest.h
+++ host/libs/StrataComm/test/ClientsControllerTest.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QObject>
+#include <QSignalSpy>
 
 #include "QtTest.h"
 #include "../src/ClientsController.h"
@@ -10,7 +11,16 @@ class ClientsControllerTest : public QObject {
     Q_OBJECT
 public:
 
-private slots:
-    void testFunction1();
+public slots:
+    void notifyClientMock(const Client &client, const QJsonObject &payload);
 
+private slots:
+    void testIsRegisteredClient();
+    void testRegisterClient();
+    void testRegisterDublicateClient();
+    void testUnregisterClient();
+    void testNotifyAllCleints();
+
+private:
+    ClientsController *clientsController_;
 };
