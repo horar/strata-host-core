b04229a1c40535679a6c4864e927b2f689fb1986
Mohammed Algadhib <mohammed.algadhib@onsemi.com>
2021-01-06 12:28:20 -0800

    format ClientsController.h

--- host/libs/StrataComm/src/ClientsController.h
+++ host/libs/StrataComm/src/ClientsController.h
@@ -31,39 +31,43 @@ public:
      * @return QList of Client objects of all connected clients.
      */
     QList<Client> getAllClients();
-    
+
     /**
      * Checks if a client is connected base on client ID.
      * @param [in] clientId client if to search for.
      * @return True if the client id is registered, false otherwise.
      */
     bool isRegisteredClient(const QByteArray &clientId);
-    
+
     /**
      * Register a client by adding it to connected clients list.
      * @param [in] client Client object containing the client id and API version.
-     * @return True if the client was registered successfully, False if a client with the same client id is already registered.
+     * @return True if the client was registered successfully, False if a client with the same
+     * client id is already registered.
      */
     bool registerClient(const Client &client);
 
     /**
      * Unregister a client by removing it from connected clients list.
      * @param [in] clientId client id to unregister.
-     * @return True if the client was found in the registered clients list and was removed successfully. False otherwise.
+     * @return True if the client was found in the registered clients list and was removed
+     * successfully. False otherwise.
      */
     bool unregisterClient(const QByteArray &clientId);
 
     /**
      * Returns client's API version.
      * @param [in] clientId client id to search for.
-     * @return ApiVersion enum of the API version. This will return ApiVersion::none if the client is not registered.
+     * @return ApiVersion enum of the API version. This will return ApiVersion::none if the client
+     * is not registered.
      */
     ApiVersion getClientApiVersion(const QByteArray &clientId);
-    
+
     /**
      * Returns the Client object based on the client id
      * @param [in] clientId client id to search for.
-     * @return Client object. if the client is not registered it will return a client object with empty client id.
+     * @return Client object. if the client is not registered it will return a client object with
+     * empty client id.
      */
     Client getClient(const QByteArray &clientId);
 
