17c10634bd23e3878ae6c1bc5c8c468d01ce1d05
Austin Foy <afoy95@live.com>
2021-02-05 11:58:59 -0800

    merged in develop

--- host/extern/ProjectExternals_monaco_editor.cmake
+++ host/extern/ProjectExternals_monaco_editor.cmake
@@ -13,7 +13,8 @@ set(MONACO_TAG 0.20.0)
         URL https://registry.npmjs.org/monaco-editor/-/monaco-editor-${MONACO_TAG}.tgz
         URL_HASH SHA1=5d5009343a550124426cb4d965a4d27a348b4dea
         CONFIGURE_COMMAND ""
-        COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/monaco.d.ts ${CMAKE_CURRENT_SOURCE_DIR}/../components/monaco/qml/tech/strata/monaco/minified/monaco.d.ts          
+        COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/monaco.d.ts ${CMAKE_CURRENT_SOURCE_DIR}/../components/monaco/qml/tech/strata/monaco/minified/monaco.d.ts
+        COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/qtQuick.js ${CMAKE_CURRENT_SOURCE_DIR}/../components/monaco/qml/tech/strata/monaco/minified/qtQuick.js      
         COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/../editor.html ${CMAKE_CURRENT_SOURCE_DIR}/../components/monaco/qml/tech/strata/monaco/minified/editor.html                    
         COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/min/vs ${CMAKE_CURRENT_SOURCE_DIR}/../components/monaco/qml/tech/strata/monaco/minified/min/vs 
         COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/min-maps/vs ${CMAKE_CURRENT_SOURCE_DIR}/../components/monaco/qml/tech/strata/monaco/minified/min-maps/vs 
