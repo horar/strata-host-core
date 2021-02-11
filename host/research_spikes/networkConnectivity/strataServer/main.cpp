12daf7bc7e980cb9ead229b0f5d65054ede63c30
Mohammed Algadhib <mohammed.algadhib@onsemi.com>
2021-02-11 15:52:46 -0800

    fix qml ReferenceError

--- host/research_spikes/networkConnectivity/strataServer/main.cpp
+++ host/research_spikes/networkConnectivity/strataServer/main.cpp
@@ -6,20 +6,20 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
     QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
-
     Server server_;
+
+    QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("Server", &server_);
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(
+        &engine, &QQmlApplicationEngine::objectCreated, &app,
+        [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl) QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection);
+    engine.load(url);
 
     return app.exec();
 }
