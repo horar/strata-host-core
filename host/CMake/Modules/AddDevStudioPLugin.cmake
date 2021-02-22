7424796ac983e36c969cba423d640dca56638b8d
Lubomir Carik <Lubomir.Carik@onsemi.com>
2021-02-22 19:09:36 +0100

    SDS: CMake macro to handle rcc plugins aka features

---
+++ host/CMake/Modules/AddDevStudioPLugin.cmake
@@ -0,0 +1,25 @@
+macro(add_sds_plugin)
+    set(options PROJ_NAME PLUGIN_OPTION_SUFFIX PLUGIN_NAME)
+    cmake_parse_arguments(local "" "${options}" "" ${ARGN})
+
+    target_sources(${local_PROJ_NAME} PRIVATE
+        qml-dev-studio-plugin-${local_PLUGIN_NAME}-static.qrc
+    )
+
+    if(NOT APPS_CORESW_SDS OR NOT ${APPS_CORESW_SDS_PLUGIN_${local_PLUGIN_OPTION_SUFFIX}})
+        if(EXISTS ${CMAKE_PLUGINS_OUTPUT_DIRECTORY}/sds-${local_PLUGIN_NAME}.rcc)
+            message(STATUS "...removing 'sds-${local_PLUGIN_NAME}.rcc' plugin")
+            file(REMOVE ${CMAKE_PLUGINS_OUTPUT_DIRECTORY}/sds-${local_PLUGIN_NAME}.rcc)
+        endif()
+    else()
+        message(STATUS "Strata DevStudio plugin '${local_PLUGIN_NAME}'...")
+
+
+        qt5_add_binary_resources(${local_PROJ_NAME}-plugin-${local_PLUGIN_NAME}
+            qml-dev-studio-plugin-${local_PLUGIN_NAME}.qrc
+            OPTIONS ARGS --compress 9 --threshold 0 --verbose
+            DESTINATION ${CMAKE_PLUGINS_OUTPUT_DIRECTORY}/sds-${local_PLUGIN_NAME}.rcc
+        )
+        add_dependencies(${local_PROJ_NAME} ${local_PROJ_NAME}-plugin-${local_PLUGIN_NAME})
+    endif()
+endmacro()
