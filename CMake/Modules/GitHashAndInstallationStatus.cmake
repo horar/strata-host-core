74ca9573b60cc217eae6110071e8d474e22e4595
Mustafa Alshehab <Mustafa.Alshehab@onsemi.com>
2021-03-31 16:02:53 -0700

    updates path to refelect on the new structure and resoves an issue where git command is not found

--- CMake/Modules/GitHashAndInstallationStatus.cmake
+++ CMake/Modules/GitHashAndInstallationStatus.cmake
@@ -1,4 +1,9 @@
 macro(get_git_hash_and_installation_status GIT_REPO_PATH LIB_INSTALLATION_PATH)
+    find_package(Git)
+    if(NOT Git_FOUND)
+        message(FATAL_ERROR "'git' program not found")
+    endif()
+
     if(${ARGC} GREATER 2)
         set(REF "remotes/origin/${ARGV2}")
     else()
@@ -9,12 +14,11 @@ macro(get_git_hash_and_installation_status GIT_REPO_PATH LIB_INSTALLATION_PATH)
         COMMAND ${GIT_EXECUTABLE} rev-parse --short=5 ${REF}
         WORKING_DIRECTORY ${GIT_REPO_PATH}
         OUTPUT_VARIABLE GIT_HASH
-        RESULTS_VARIABLE GIT_HASH_RESULT
-        OUTPUT_STRIP_TRAILING_WHITESPACE
+        RESULT_VARIABLE GIT_HASH_RESULT
     )
 
     if(NOT ${GIT_HASH_RESULT} EQUAL 0)
-        message(FATAL_ERROR "Failed to get git hash for ${GIT_REPO_PATH}. GIT_HASH_RESULT is: ${GIT_HASH_RESULT}")
+        message(FATAL_ERROR "Failed to get git hash for ${GIT_REPO_PATH}\nGIT_HASH_RESULT: ${GIT_HASH_RESULT}")
     endif()
 
     # checks for header files in a given path
