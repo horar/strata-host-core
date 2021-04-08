584328fe0ff093c39a6a1330832dcd5cc9bf0039
Martin Michalek <martin.michalek@onsemi.com>
2021-04-08 11:20:40 +0200

    SGUtilsCpp: new method keySequenceNativeText()

--- components/commoncpp/SGUtilsCpp/include/SGUtilsCpp.h
+++ components/commoncpp/SGUtilsCpp/include/SGUtilsCpp.h
@@ -39,6 +39,7 @@ public:
     Q_INVOKABLE static bool validateJson(const QByteArray &json, const QByteArray &schema);
     Q_INVOKABLE static QString toHex(qint64 number, int width = 0);
     Q_INVOKABLE static void copyToClipboard(const QString &text);
+    Q_INVOKABLE static QString keySequenceNativeText(QString sequence);
 
 private:
     const QStringList fileSizePrefixList_;
