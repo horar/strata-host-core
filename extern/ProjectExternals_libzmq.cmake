a8a55f159f657327b31b140bf6c576dc2dcd279a
Luay Alshawi <luay.alshawi@onsemi.com>
2021-04-26 17:28:48 -0700

    improve libzmq  dynamic library copying to bin by addign custom external project step

--- extern/ProjectExternals_libzmq.cmake
+++ extern/ProjectExternals_libzmq.cmake
@@ -16,68 +16,69 @@ function(ReadLibzmqSoVersion)
     mark_as_advanced(LIBZMQ_SOVERSION)
 endfunction()
 
-if(NOT LIB_INSTALLED)
-    file(MAKE_DIRECTORY ${EXTERN_INSTALL_DIR_PATH}/libzmq-${GIT_HASH}/include)
-    file(MAKE_DIRECTORY ${EXTERN_INSTALL_DIR_PATH}/libzmq-${GIT_HASH}/lib)
+file(MAKE_DIRECTORY ${EXTERN_INSTALL_DIR_PATH}/libzmq-${GIT_HASH}/include)
+file(MAKE_DIRECTORY ${EXTERN_INSTALL_DIR_PATH}/libzmq-${GIT_HASH}/lib)
 
-    if(WIN32)
-        ExternalProject_Add(libzmq
-            INSTALL_DIR ${EXTERN_INSTALL_DIR_PATH}/libzmq-${GIT_HASH}
-            SOURCE_DIR ${SOURCE_DIR_EXTERN}/libzmq
-            EXCLUDE_FROM_ALL ON
-            CMAKE_ARGS "${CMAKE_ARGS}"
-                -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-                -DCMAKE_PDB_OUTPUT_DIRECTORY=<INSTALL_DIR>/bin
-                -DWITH_PERF_TOOL=OFF
-                -DZMQ_BUILD_TESTS=OFF
-                -DENABLE_CPACK=OFF
+if(WIN32)
+    ExternalProject_Add(libzmq
+        INSTALL_DIR ${EXTERN_INSTALL_DIR_PATH}/libzmq-${GIT_HASH}
+        SOURCE_DIR ${SOURCE_DIR_EXTERN}/libzmq
+        EXCLUDE_FROM_ALL ON
+        CMAKE_ARGS "${CMAKE_ARGS}"
+            -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+            -DCMAKE_PDB_OUTPUT_DIRECTORY=<INSTALL_DIR>/bin
+            -DWITH_PERF_TOOL=OFF
+            -DZMQ_BUILD_TESTS=OFF
+            -DENABLE_CPACK=OFF
 
-            PATCH_COMMAND ${GIT_EXECUTABLE} reset --hard && ${GIT_EXECUTABLE} apply --verbose --ignore-space-change --ignore-whitespace ${CMAKE_CURRENT_SOURCE_DIR}/patches/libzmq/jom-build-support.patch
-            && ${GIT_EXECUTABLE} apply --verbose --ignore-space-change --ignore-whitespace ${CMAKE_CURRENT_SOURCE_DIR}/patches/libzmq/remove-library-name-postfix.patch
+        COPY_DYNAMIC_LIB_TO_BIN
 
-            INSTALL_COMMAND ${CMAKE_COMMAND} --build . --target install
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different <INSTALL_DIR>/bin/libzmq$<$<CONFIG:DEBUG>:d>${CMAKE_SHARED_LIBRARY_SUFFIX} ${CMAKE_BINARY_DIR}/bin
-        )
-    else()
-        ReadLibzmqSoVersion()
-        ExternalProject_Add(libzmq
-            INSTALL_DIR ${EXTERN_INSTALL_DIR_PATH}/libzmq-${GIT_HASH}
-            SOURCE_DIR ${SOURCE_DIR_EXTERN}/libzmq
-            EXCLUDE_FROM_ALL ON
-            CMAKE_ARGS "${CMAKE_ARGS}"
-                -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-                -DCMAKE_MACOSX_RPATH=1
-                -DWITH_PERF_TOOL=OFF
-                -DZMQ_BUILD_TESTS=OFF
-                -DENABLE_CPACK=OFF
+        PATCH_COMMAND ${GIT_EXECUTABLE} reset --hard && ${GIT_EXECUTABLE} apply --verbose --ignore-space-change --ignore-whitespace ${CMAKE_CURRENT_SOURCE_DIR}/patches/libzmq/jom-build-support.patch
+        && ${GIT_EXECUTABLE} apply --verbose --ignore-space-change --ignore-whitespace ${CMAKE_CURRENT_SOURCE_DIR}/patches/libzmq/remove-library-name-postfix.patch
 
-            INSTALL_COMMAND ${CMAKE_COMMAND} --build . --target install
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different ${EXTERN_INSTALL_DIR_PATH}/libzmq-${GIT_HASH}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}zmq${CMAKE_SHARED_LIBRARY_SUFFIX} ${CMAKE_BINARY_DIR}/bin
-            COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_BINARY_DIR}/bin/${CMAKE_SHARED_LIBRARY_PREFIX}zmq${CMAKE_SHARED_LIBRARY_SUFFIX} ${CMAKE_BINARY_DIR}/bin/${CMAKE_SHARED_LIBRARY_PREFIX}zmq.${LIBZMQ_SOVERSION}${CMAKE_SHARED_LIBRARY_SUFFIX}
-        )
-    endif()
+        INSTALL_COMMAND ${CMAKE_COMMAND} --build . --target install
+    )
 else()
-    # TODO: RS: File output build dependency for ExternalProject_Add in CMake
-    # https://jira.onsemi.com/browse/CS-1442
-    if(WIN32)
-        if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-            execute_process(
-                COMMAND ${CMAKE_COMMAND} -E copy_if_different ${EXTERN_INSTALL_DIR_PATH}/libzmq-${GIT_HASH}/bin/libzmqd${CMAKE_SHARED_LIBRARY_SUFFIX} ${CMAKE_BINARY_DIR}/bin
-            )
-        else()
-            execute_process(
-                COMMAND ${CMAKE_COMMAND} -E copy_if_different ${EXTERN_INSTALL_DIR_PATH}/libzmq-${GIT_HASH}/bin/libzmq${CMAKE_SHARED_LIBRARY_SUFFIX} ${CMAKE_BINARY_DIR}/bin
-            )
-        endif()
-    else()
-        # re-run the detection in case user keeps swapping branches with different libzmq libraries
-        ReadLibzmqSoVersion()
-        execute_process(
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different ${EXTERN_INSTALL_DIR_PATH}/libzmq-${GIT_HASH}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}zmq${CMAKE_SHARED_LIBRARY_SUFFIX} ${CMAKE_BINARY_DIR}/bin
-            COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_BINARY_DIR}/bin/${CMAKE_SHARED_LIBRARY_PREFIX}zmq${CMAKE_SHARED_LIBRARY_SUFFIX} ${CMAKE_BINARY_DIR}/bin/${CMAKE_SHARED_LIBRARY_PREFIX}zmq.${LIBZMQ_SOVERSION}${CMAKE_SHARED_LIBRARY_SUFFIX}
-        )
-    endif()
+    ReadLibzmqSoVersion()
+    ExternalProject_Add(libzmq
+        INSTALL_DIR ${EXTERN_INSTALL_DIR_PATH}/libzmq-${GIT_HASH}
+        SOURCE_DIR ${SOURCE_DIR_EXTERN}/libzmq
+        EXCLUDE_FROM_ALL ON
+        CMAKE_ARGS "${CMAKE_ARGS}"
+            -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+            -DCMAKE_MACOSX_RPATH=1
+            -DWITH_PERF_TOOL=OFF
+            -DZMQ_BUILD_TESTS=OFF
+            -DENABLE_CPACK=OFF
+
+        INSTALL_COMMAND ${CMAKE_COMMAND} --build . --target install
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different ${EXTERN_INSTALL_DIR_PATH}/libzmq-${GIT_HASH}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}zmq${CMAKE_SHARED_LIBRARY_SUFFIX} ${CMAKE_BINARY_DIR}/bin
+        COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_BINARY_DIR}/bin/${CMAKE_SHARED_LIBRARY_PREFIX}zmq${CMAKE_SHARED_LIBRARY_SUFFIX} ${CMAKE_BINARY_DIR}/bin/${CMAKE_SHARED_LIBRARY_PREFIX}zmq.${LIBZMQ_SOVERSION}${CMAKE_SHARED_LIBRARY_SUFFIX}
+    )
 endif()
+
+
+if(WIN32)
+    set(ZMQ_DYNAMIC_LIB_PATH ${EXTERN_INSTALL_DIR_PATH}/libzmq-${GIT_HASH}/bin/libzmq$<$<CONFIG:Debug>:d>${CMAKE_SHARED_LIBRARY_SUFFIX})
+    ExternalProject_Add_Step(libzmq COPY_DYNAMIC_LIB_TO_BIN
+            COMMENT "Copy libzmq dynamic library from ${ZMQ_DYNAMIC_LIB_PATH} to ${CMAKE_BINARY_DIR}/bin"
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different ${ZMQ_DYNAMIC_LIB_PATH} ${CMAKE_BINARY_DIR}/bin
+            ALWAYS ON
+            DEPENDEES install
+    )
+else()
+    # re-run the detection in case user keeps swapping branches with different libzmq libraries
+    ReadLibzmqSoVersion()
+    set(ZMQ_DYNAMIC_LIB_PATH ${EXTERN_INSTALL_DIR_PATH}/libzmq-${GIT_HASH}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}zmq${CMAKE_SHARED_LIBRARY_SUFFIX})
+    ExternalProject_Add_Step(libzmq COPY_DYNAMIC_LIB_TO_BIN
+            COMMENT "Copy libzmq dynamic library from ${ZMQ_DYNAMIC_LIB_PATH} to ${CMAKE_BINARY_DIR}/bin"
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different ${ZMQ_DYNAMIC_LIB_PATH} ${CMAKE_BINARY_DIR}/bin
+            COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_BINARY_DIR}/bin/${CMAKE_SHARED_LIBRARY_PREFIX}zmq${CMAKE_SHARED_LIBRARY_SUFFIX} ${CMAKE_BINARY_DIR}/bin/${CMAKE_SHARED_LIBRARY_PREFIX}zmq.${LIBZMQ_SOVERSION}${CMAKE_SHARED_LIBRARY_SUFFIX}
+            ALWAYS ON
+            DEPENDEES install
+    )
+endif()
+
 
 add_library(zeromq::libzmq SHARED IMPORTED GLOBAL)
 
