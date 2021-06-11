28ca3923e28bda4b00c4a1b7e8bfbc98fbae0e0d
zbb69r <austin.foy@onsemi.com>
2021-06-11 15:05:42 -0700

    added patch command

--- extern/monaco-editor-0.20.0/package/esm/vs/basic-languages/qml/qml.contribution.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-import { registerLanguage } from '../_.contribution.js';
-registerLanguage({ 
-    id: 'qml' ,
-    extensions:[".qml"],
-    aliases: ["qml","QML"],
-    loader: function () { return import('./qml.js'); }
-})
