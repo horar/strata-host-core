7fe5bac87c4f932970014eb73185854118d37ad3
Filip Kozak <filip.kozak@onsemi.com>
2021-06-14 16:06:50 +0200

    CS-1954: Flasher: Fail flashing process if firmware is unable to start

--- libs/Board/Platform/src/Operations/StartApplication.cpp
+++ libs/Board/Platform/src/Operations/StartApplication.cpp
@@ -1,4 +1,5 @@
 #include <Operations/StartApplication.h>
+#include <PlatformOperationsStatus.h>
 #include "Commands/PlatformCommands.h"
 #include "PlatformOperationsConstants.h"
 
@@ -7,6 +8,7 @@ namespace strata::platform::operation {
 using command::CmdStartApplication;
 using command::CmdRequestPlatformId;
 using command::CmdGetFirmwareInfo;
+using command::CommandResult;
 
 StartApplication::StartApplication(const PlatformPtr& platform) :
     BasePlatformOperation(platform, Type::StartApplication)
@@ -14,11 +16,29 @@ StartApplication::StartApplication(const PlatformPtr& platform) :
     commandList_.reserve(3);
 
     // BasePlatformOperation member platform_ must be used as a parameter for commands!
-    commandList_.emplace_back(std::make_unique<CmdStartApplication>(platform_));
-    commandList_.emplace_back(std::make_unique<CmdGetFirmwareInfo>(platform_, true, MAX_GET_FW_INFO_RETRIES));
-    commandList_.emplace_back(std::make_unique<CmdRequestPlatformId>(platform_));
+    commandList_.emplace_back(std::make_unique<CmdStartApplication>(platform_));                                // 0
+    commandList_.emplace_back(std::make_unique<CmdGetFirmwareInfo>(platform_, true, MAX_GET_FW_INFO_RETRIES));  // 1
+    commandList_.emplace_back(std::make_unique<CmdRequestPlatformId>(platform_));                               // 2
 
     initCommandList();
+
+    // After calling 'request_platform_id' command, we need to check if board
+    // is already in application mode. If no, we set data for 'finished' signal
+    // to 'FIRMWARE_UNABLE_START' in method 'postCommandActions()'.
+    reqPlatfIdCmdIter_ = commandList_.begin() + 2;
+    postCommandHandler_ = std::bind(&StartApplication::postCommandActions, this, std::placeholders::_1, std::placeholders::_2);
+}
+
+void StartApplication::postCommandActions(CommandResult& result, int& status)
+{
+    Q_UNUSED(result)
+
+    if (currentCommand_ == reqPlatfIdCmdIter_) {
+        if (BasePlatformOperation::bootloaderMode() == true) {
+            // set status for 'finished' signal
+            status = FIRMWARE_UNABLE_START;
+        }
+    }
 }
 
 }  // namespace
