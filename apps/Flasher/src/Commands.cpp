b0a301419c74d66b8eb95072e3a581846002ed82
Filip Kozak <filip.kozak@onsemi.com>
2021-08-12 15:38:01 +0200

    CS-2245: Fixed state when FlasherCLI gets stuck and stays open

--- apps/Flasher/src/Commands.cpp
+++ apps/Flasher/src/Commands.cpp
@@ -105,31 +105,44 @@ bool DeviceCommand::createSerialDevice() {
 }
 
 void DeviceCommand::handleDeviceError(device::Device::ErrorCode errCode, QString errStr) {
-    Q_UNUSED(errStr)
+    switch (errCode) {
+    case device::Device::ErrorCode::NoError :
+        break;
+    case device::Device::ErrorCode::DeviceFailedToOpen :
+        {
+            ++openRetries_;
+            QString errorMessage(QStringLiteral("Cannot open board (serial device) "));
+            errorMessage.append(platform_->deviceName());
+            errorMessage.append(QStringLiteral(", attempt "));
+            errorMessage.append(QString::number(openRetries_));
+            errorMessage.append(QStringLiteral(" of "));
+            errorMessage.append(QString::number(OPEN_MAX_RETRIES));
 
-    if (errCode == device::Device::ErrorCode::DeviceFailedToOpen) {
-        ++openRetries_;
-        QString errorMessage(QStringLiteral("Cannot open board (serial device) "));
-        errorMessage.append(platform_->deviceName());
-        errorMessage.append(QStringLiteral(", attempt "));
-        errorMessage.append(QString::number(openRetries_));
-        errorMessage.append(QStringLiteral(" of "));
-        errorMessage.append(QString::number(OPEN_MAX_RETRIES));
-
-        if (openRetries_ >= OPEN_MAX_RETRIES) {
-            qCCritical(logCategoryFlasherCli).noquote() << errorMessage;
-            emit finished(EXIT_FAILURE);
-            return;
-        } else {
-            qCInfo(logCategoryFlasherCli).noquote() << errorMessage;
+            if (openRetries_ >= OPEN_MAX_RETRIES) {
+                qCCritical(logCategoryFlasherCli).noquote() << errorMessage;
+                emit finished(EXIT_FAILURE);
+            } else {
+                qCInfo(logCategoryFlasherCli).noquote() << errorMessage;
+            }
         }
+        break;
+    case device::Device::ErrorCode::DeviceDisconnected :
+    case device::Device::ErrorCode::DeviceError :
+        qCCritical(logCategoryFlasherCli).noquote() << QStringLiteral("Device error:") << errStr;
+        emit criticalDeviceError();
+        break;
     }
 }
 
 // FLASHER (FLASH/BACKUP firmware/bootloader) command
 
 FlasherCommand::FlasherCommand(const QString &fileName, int deviceNumber, CmdType command) :
-    DeviceCommand(deviceNumber), fileName_(fileName), command_(command) { }
+    DeviceCommand(deviceNumber),
+    fileName_(fileName),
+    command_(command)
+{
+    connect(this, &DeviceCommand::criticalDeviceError, this, &FlasherCommand::handleCriticalDeviceError);
+}
 
 // Destructor must be defined due to unique pointer to incomplete type.
 FlasherCommand::~FlasherCommand() { }
@@ -163,10 +176,20 @@ void FlasherCommand::handlePlatformOpened() {
     }
 }
 
+void FlasherCommand::handleCriticalDeviceError() {
+    // Commands in flasher reacts on Device errors, so handle them only if flasher is not created yet
+    if (flasher_ == nullptr) {
+        emit finished(EXIT_FAILURE);
+    }
+}
+
 // INFO command
 
 InfoCommand::InfoCommand(int deviceNumber) :
-    DeviceCommand(deviceNumber) { }
+    DeviceCommand(deviceNumber)
+{
+    connect(this, &DeviceCommand::criticalDeviceError, this, &InfoCommand::handleCriticalDeviceError);
+}
 
 // Destructor must be defined due to unique pointer to incomplete type.
 InfoCommand::~InfoCommand() { }
@@ -244,6 +267,13 @@ void InfoCommand::handleIdentifyOperationFinished(platform::operation::Result re
         qCWarning(logCategoryFlasherCli) << "Identify operation failed:" << errStr;
         emit finished(EXIT_FAILURE);
     } break;
+    }
+}
+
+void InfoCommand::handleCriticalDeviceError() {
+    // Commands in identify operation reacts on Device errors, so handle them only if this operation is not created yet
+    if (identifyOperation_ == nullptr) {
+        emit finished(EXIT_FAILURE);
     }
 }
 
