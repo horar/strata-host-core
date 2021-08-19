7163a1fa80cff90a4ef145e83cd13c62d392dfce
Lubomir Carik <lubomir.carik@onsemi.com>
2021-08-19 15:46:44 +0200

    loggers: introduce log-line predefined contants

---
+++ libs/loggers/QtLogger/include/QtLoggerConstants.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace strata::loggers::contants
+{
+constexpr char LOGLINE_CHAR_MAJOR{'='};
+constexpr char LOGLINE_CHAR_MINOR{'-'};
+constexpr int LOGLINE_LENGTH{78};
+
+}  // namespace strata::loggers::contants
