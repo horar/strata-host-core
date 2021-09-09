be12eaeb163f55a532ba1761248d5b2cd188a98f
Mohammed Algadhib <mohammed.algadhib@onsemi.com>
2021-09-09 11:13:17 -0700

    SDS: fix copy pase typo

--- apps/DeveloperStudio/main.cpp
+++ apps/DeveloperStudio/main.cpp
@@ -151,7 +151,7 @@ int main(int argc, char *argv[])
     qmlRegisterUncreatableType<PlatformInterfaceGenerator>("tech.strata.PlatformInterfaceGenerator", 1, 0, "PlatformInterfaceGenerator", "You can't instantiate PlatformInterfaceGenerator in QML");
     qmlRegisterUncreatableType<SDSModel>("tech.strata.SDSModel", 1, 0, "SDSModel", "You can't instantiate SDSModel in QML");
     qmlRegisterUncreatableType<VisualEditorUndoStack>("tech.strata.VisualEditorUndoStack", 1, 0, "VisualEditorUndoStack", "You can't instantiate VisualEditorUndoStack in QML");
-    qmlRegisterUncreatableType<strata::strataRPC::StrataClient>("tech.strata.StrataClient", 1, 0, "StrataClient", QStringLiteral("You can't instantiate DocumentManager in QML"));
+    qmlRegisterUncreatableType<strata::strataRPC::StrataClient>("tech.strata.StrataClient", 1, 0, "StrataClient", QStringLiteral("You can't instantiate StrataClient in QML"));
     qmlRegisterInterface<strata::strataRPC::DeferredRequest>("DeferredRequest");
 
     // [LC] QTBUG-85137 - doesn't reconnect on Linux; fixed in further 5.12/5.15 releases
