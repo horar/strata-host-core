9b79008c29a9502b08c23db4e3b61caa431bfada
Sona Zajicova <sona.zajicova@onsemi.com>
2021-10-19 17:34:42 +0200

    Empty window for Logging Configuration Utility app

---
+++ apps/LoggingConfigurationUtility/main.cpp
@@ -0,0 +1,113 @@
+/***************************************************************************
+  Copyright (c) 2018-2021 onsemi.
+
+   All rights reserved. This software and/or documentation is licensed by onsemi under
+   limited terms and conditions. The terms and conditions pertaining to the software and/or
+   documentation are available at http://www.onsemi.com/site/pdf/ONSEMI_T&C.pdf (“onsemi Standard
+   Terms and Conditions of Sale, Section 8 Software”).
+   ***************************************************************************/
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QtQml/QQmlContext>
+#include <QSettings>
+#include <QResource>
+#include <QDir>
+#include <QIcon>
+#include <QQmlFileSelector>
+
+#include <QtLoggerSetup.h>
+#include "logging/LoggingQtCategories.h"
+
+void loadResources() {
+    QDir applicationDir(QCoreApplication::applicationDirPath());
+
+    const auto resources = {
+        QStringLiteral("component-fonts.rcc"),
+        QStringLiteral("component-common.rcc"),
+        QStringLiteral("component-sgwidgets.rcc"),
+        QStringLiteral("component-theme.rcc")};
+
+#ifdef Q_OS_MACOS
+    applicationDir.cdUp();
+    applicationDir.cdUp();
+    applicationDir.cdUp();
+#endif
+
+    for (const auto& resourceName : resources) {
+        QString resourcePath = applicationDir.filePath(resourceName);
+
+        qCInfo(logCategoryLoggingConfigurationUtility)
+                << "Loading"
+                << resourceName << ":"
+                << QResource::registerResource(resourcePath);
+    }
+}
+
+void addImportPaths(QQmlApplicationEngine *engine) {
+    QDir applicationDir(QCoreApplication::applicationDirPath());
+
+#ifdef Q_OS_MACOS
+    applicationDir.cdUp();
+    applicationDir.cdUp();
+    applicationDir.cdUp();
+#endif
+
+    bool status = applicationDir.cd("imports");
+    if (status == false) {
+        qCCritical(logCategoryLoggingConfigurationUtility) << "failed to find import path.";
+    }
+
+    engine->addImportPath(applicationDir.path());
+
+    engine->addImportPath("qrc:///");
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setOrganizationName(QStringLiteral("onsemi"));
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QSettings::setDefaultFormat(QSettings::IniFormat);
+
+    QGuiApplication app(argc, argv);
+//    app.setWindowIcon(QIcon(":/images/prt-logo.svg"));
+
+    const strata::loggers::QtLoggerSetup loggerInitialization(app);
+    qCInfo(logCategoryLoggingConfigurationUtility()) << QStringLiteral("%1 v%2").arg(QCoreApplication::applicationName()).arg(QCoreApplication::applicationVersion());
+
+    loadResources();
+
+    QQmlApplicationEngine engine;
+    QQmlFileSelector selector(&engine);
+
+    addImportPaths(&engine);
+
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty()) {
+        return -1;
+    }
+
+    return app.exec();
+}
+
+/*int main(int argc, char *argv[])
+{
+    qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
+
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}*/
---
+++ apps/LoggingConfigurationUtility/main.qml
@@ -0,0 +1,12 @@
+import QtQuick 2.12
+import QtQuick.Window 2.12
+
+Window {
+    id: window
+    width: 640
+    height: 480
+    visible: true
+    title: qsTr("Logging Configuration Utility")
+
+
+}
